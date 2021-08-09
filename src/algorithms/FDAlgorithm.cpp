5ba5464de5557031a73fe80fb60ca51522841e88
vs9lh <zr9ihi@gmail.com>
2021-08-09 21:26:28 +0300

    Fix: Changed functions for data output, added new attribute in table, refactoring

--- src/algorithms/FDAlgorithm.cpp
+++ src/algorithms/FDAlgorithm.cpp
@@ -5,12 +5,17 @@ std::vector<std::string> FDAlgorithm::getColumnNames() {
     return inputGenerator_.getColumnNames();
 }
 
-std::string FDAlgorithm::getJsonFDs() {
+std::string FDAlgorithm::getJsonFDs(bool withNullLhs) {
     nlohmann::json j = nlohmann::json::array();
 
     fdCollection_.sort();
     for (auto& fd : fdCollection_) {
-        j.push_back(fd.toJSON());
+        if (withNullLhs) {
+            j.push_back(fd.toJSON());
+        } else {
+            if (fd.getLhs().getArity() != 0)
+                j.push_back(fd.toJSON());
+        }
     }
     return j.dump();
 }
@@ -18,8 +23,13 @@ std::string FDAlgorithm::getJsonFDs() {
 std::string FDAlgorithm::getJsonArrayNameValue(int degree, bool withAttr) {
     size_t numberOfColumns = inputGenerator_.getNumberOfColumns();
     auto columnNames = inputGenerator_.getColumnNames();
-    std::vector<double> LhsValues(numberOfColumns, 0);
-    std::vector<double> RhsValues(numberOfColumns, 0);
+
+    std::vector<std::pair<double, int>> LhsValues(numberOfColumns);
+    std::vector<std::pair<double, int>> RhsValues(numberOfColumns);
+    
+    for (size_t i = 0; i != numberOfColumns; ++i) {
+        LhsValues[i] = RhsValues[i] = { 0, i };
+    }
 
     for (const auto &fd : fdCollection_) {
         double divisor = std::pow(fd.getLhs().getArity(), degree);
@@ -28,23 +38,37 @@ std::string FDAlgorithm::getJsonArrayNameValue(int degree, bool withAttr) {
         for (size_t index = LhsColumnIndices.find_first();
             index != boost::dynamic_bitset<>::npos;
             index = LhsColumnIndices.find_next(index)) {
-                LhsValues[index] += 1/divisor;
+                LhsValues[index].first += 1/divisor;
         }
-        const auto &RhsColumn = fd.getRhs();
-        size_t index = RhsColumn.getIndex();
+        size_t index = fd.getRhs().getIndex();
+
         if (divisor != 0)
-            RhsValues[index] += 1/divisor;
+            RhsValues[index].first += 1/divisor;
         else
-            RhsValues[index] = -1;
+            RhsValues[index].first = -1;
     }
+
+    auto pair_greater = [](std::pair<double, int> a, std::pair<double, int> b) {
+        return a.first > b.first;
+    };
+
+    std::sort(LhsValues.begin(), LhsValues.end(), pair_greater);
+    std::sort(RhsValues.begin(), RhsValues.end(), pair_greater);
+
     nlohmann::json j;
 
     std::vector<std::pair<nlohmann::json, nlohmann::json>> lhs_array;
     std::vector<std::pair<nlohmann::json, nlohmann::json>> rhs_array;
-    for (size_t i=0; i!= numberOfColumns; ++i) {
-        auto name = withAttr ? columnNames[i] : std::to_string(i);
-        lhs_array.push_back({{"name", name}, {"value", LhsValues[i]}});
-        rhs_array.push_back({{"name", name}, {"value", RhsValues[i]}});
+
+    for (size_t i = 0; i != numberOfColumns; ++i) {
+        auto name = withAttr ? columnNames[LhsValues[i].second] : std::string("Attribute " + i);
+        if (LhsValues[i].first > 0) {
+            lhs_array.push_back({{"name", name}, {"value", LhsValues[i].first}});
+        }
+        name = withAttr ? columnNames[RhsValues[i].second] : std::string("Attribute " + i);
+        if (RhsValues[i].first > 0) {
+            rhs_array.push_back({{"name", name}, {"value", RhsValues[i].first}});
+        }
     }
     
     j["lhs"] = lhs_array;
@@ -67,38 +91,7 @@ unsigned int FDAlgorithm::Fletcher16() {
     return (sum2 << 8) | sum1;
 }
 
-/* Attribute A contains only unique values (i.e. A is the key) iff [A]->[B]
- * holds for every attribute B. So to determine if A is a key, we count
- * number of fds with lhs==[A] and if it equals the total number of attributes
- * minus one (the attribute A itself) then A is the key.
- */
-std::vector<Column const*> FDAlgorithm::GetKeys() const {
-    std::vector<Column const*> keys;
-    std::map<Column const*, size_t> fds_count_per_col;
-    unsigned int cols_of_equal_values = 0;
-    size_t const number_of_cols = input_generator_.GetNumberOfColumns();
-
-    for (FD const& fd : fd_collection_) {
-        Vertical const& lhs = fd.GetLhs();
-
-        if (lhs.GetArity() == 0) {
-            /* We separately count columns consisting of only equal values,
-             * because they cannot be on the right side of the minimal fd.
-             * And obviously for every attribute A true: [A]->[B] holds
-             * if []->[B] holds.
-             */
-            cols_of_equal_values++;
-        } else if (lhs.GetArity() == 1) {
-            fds_count_per_col[lhs.GetColumns().front()]++;
-        }
-    }
-
-    for (auto const&[col, num] : fds_count_per_col) {
-        if (num + 1 + cols_of_equal_values == number_of_cols) {
-            keys.push_back(col);
-        }
-    }
-
-    return keys;
+std::string FDAlgorithm::getJsonColumnNames() {
+    nlohmann::json j = nlohmann::json(inputGenerator_.getColumnNames());
+    return j.dump();
 }
-
