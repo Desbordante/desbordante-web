9747c9023e82ebf156a106a437fae15318ad463b
vs9lh <zr9ihi@gmail.com>
2021-11-14 12:24:05 +0300

    Fix: Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,30 @@
+usage() {
+cat << EOF  
+Usage: ./build.sh [-c|-consumer] [-h|--help]
+
+-h,     -help,          Display help
+
+-c,     --consumer      Enable consumer compilation
+
+EOF
+}
+
+PREFIX=""
+POSTFIX="-DCMAKE_BUILD_TYPE=RELEASE"
+
+for i in "$@"
+    do
+    case $i in
+        -c|--consumer) # Enable consumer compile
+            PREFIX="-D ENABLE_CONSUMER_COMPILE=ON"
+            ;;
+        -h|--help|*) # Display help.
+            usage
+            exit 0
+            ;;
+    esac
+done
+
 mkdir lib
 cd lib
 git clone https://github.com/google/googletest/ --branch release-1.10.0
@@ -6,5 +33,6 @@ git clone https://github.com/aantron/better-enums.git --branch 0.11.3
 cd ..
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE
+rm CMakeCache.txt
+cmake $PREFIX .. $POSTFIX
 make
