3ec1c595891fe9ec9f7c29d11181bee8ef8ee7dd
vs9lh <zr9ihi@gmail.com>
2021-11-14 23:09:50 +0300

    Fix: Fixed problem with db creating

---
+++ web-app/server/src/db/createDB.js
@@ -0,0 +1,25 @@
+const pgtools = require('pgtools');
+
+async function createDB() {
+    const config = {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+    }
+
+    await pgtools.createdb(config, process.env.DB_NAME)
+    .then((res) => {
+            console.log(`Database '${process.env.DB_NAME}' was successfully created`);
+        }
+    ).catch((err) => {
+        if (err.name === 'duplicate_database') {
+            console.log(`Database '${process.env.DB_NAME}' already exists`)
+        } else {
+            console.log(err);
+            process.exit();
+        }
+    })
+}
+
+module.exports = createDB;
