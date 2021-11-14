0f353965cee6c42143506d0d47f6718d9328fff2
vs9lh <zr9ihi@gmail.com>
2021-11-14 23:09:50 +0300

    Fix: Fixed problem with db creating

--- web-app/server/src/db/dropTable.js
+++ web-app/server/src/db/dropTable.js
@@ -1,5 +1,5 @@
 async function dropTableTasks(pool) {
-    return await pool.query(`DROP TABLE IF EXISTS ${process.env.DB_TASKS_TABLE_NAME}`)
+    await pool.query(`DROP TABLE IF EXISTS ${process.env.DB_TASKS_TABLE_NAME}`)
     .then((res) => {
         console.log(`Tables in DB '${process.env.DB_NAME}' was successfully dropped`)
     })
