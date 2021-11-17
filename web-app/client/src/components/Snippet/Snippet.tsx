95e77edfb48a03052c3451c94d58fc4b6dcee979
vs9lh <zr9ihi@gmail.com>
2021-11-17 23:50:05 +0300

    Add: Snippet processing moved to the server

--- web-app/client/src/components/Snippet/Snippet.tsx
+++ web-app/client/src/components/Snippet/Snippet.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable no-console */
 import axios from "axios";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { coloredDepedency } from "../../types";
-import { DelimeterContext } from "../DelimeterContext/DelimeterContext";
 import "./Snippet.scss";
 
 import { serverURL } from "../../APIFunctions";
 
 interface Props {
-  file: File | null;
   taskId: string;
   selectedDependency: coloredDepedency | undefined;
 }
@@ -29,74 +27,22 @@ function getSelectedIndices(
     return selectedIndices;
   }
   dep.lhs.forEach((lhs) =>
-    selectedIndices.set(header.indexOf(lhs.name), lhs.color)
-  );
+    selectedIndices.set(header.indexOf(lhs.name), lhs.color));
   selectedIndices.set(header.indexOf(dep.rhs.name), dep.rhs.color);
   return selectedIndices;
 }
 
-// function convertCSVToArray(inputData: string, delimiter: string) {
-//   const objPattern = new RegExp(
-//     "(\\" +
-//       delimiter +
-//       "|\\r?\\n|\\r|^)" +
-//       '(?:"([^"]*(?:""[^"]*)*)"|' +
-//       '([^"\\' +
-//       delimiter +
-//       "\\r\\n]*))",
-//     "gi"
-//   );
-
-//   let arrMatches: any;
-//   const arrData: string[][] = [[]];
-//   arrMatches = objPattern.exec(inputData);
-//   while (arrMatches !== null) {
-//     const strMatchedDelimiter = arrMatches[1];
-//     if (strMatchedDelimiter.length && strMatchedDelimiter !== delimiter) {
-//       arrData.push([]);
-//     }
-
-//     let strMatchedValue;
-
-//     if (arrMatches[2]) {
-//       strMatchedValue = arrMatches[2].replace(new RegExp('""', "g"), '"');
-//     } else {
-//       strMatchedValue = arrMatches[3];
-//     }
-
-//     arrData[arrData.length - 1].push(strMatchedValue);
-//     arrMatches = objPattern.exec(inputData);
-//   }
-
-//   return arrData;
-// }
-
-const Snippet: React.FC<Props> = ({ file, taskId, selectedDependency }) => {
+const Snippet: React.FC<Props> = ({ taskId, selectedDependency }) => {
   const [table, setTable] = useState<string[][]>([[]]);
   const selectedIndices: Map<number, string> = getSelectedIndices(
     selectedDependency,
     table[0]
   );
-  const delimeterContext = useContext(DelimeterContext);
-
-  // useEffect(() => {
-  //   function readFile() {
-  //     file!
-  //       .text()
-  //       .then((buffer) =>
-  //         setTable(convertCSVToArray(buffer, delimeterContext?.delimeter!))
-  //       );
-  //   }
-
-  //   if (file !== null) {
-  //     readFile();
-  //   }
-  // });
 
   useEffect(() => {
     axios
       .get(serverURL + "/getSnippet?taskID=" + taskId)
-      .then((res) => setTable(res.data.arrData));
+      .then((res) => setTable(res.data));
   }, []);
 
   return (
