b73947db92cfe9a421a9a176bfaed05cb66c19cb
Chizhov Anton <zr9ihi@gmail.com>
2021-11-25 13:41:03 +0300

    Fix: Server refactoring, added eslint

--- web-app/server/src/db/configureApp.js
+++ web-app/server/src/db/configureApp.js
@@ -1,26 +1,26 @@
-const createPool = require('./createPool');
-const createTable = require('./createTable');
-const dropTableTasks = require('./dropTable');
-const createDB = require('./createDB');
+const createPool = require("./createPool");
+const createTable = require("./createTable");
+const dropTableTasks = require("./dropTable");
+const createDB = require("./createDB");
 
 async function configureApp(app) {
   return createDB()
-  .then(async(res) => { 
-    return await createPool();
-  })
-  .then(async(pool) => {
-    await dropTableTasks(pool);
-    return pool;
-  })
-  .then(async(pool) => { 
-    app.set('pool', pool); 
-    await createTable(pool);
-    return pool;
-  })
-  .catch(async(err) => {
-    console.log(err);
-    throw err;
-  })
+      .then(async () => {
+        return await createPool();
+      })
+      .then(async (pool) => {
+        await dropTableTasks(pool);
+        return pool;
+      })
+      .then(async (pool) => {
+        app.set("pool", pool);
+        await createTable(pool);
+        return pool;
+      })
+      .catch((err) => {
+        console.error(err);
+        throw err;
+      });
 }
 
 module.exports = configureApp;
--- web-app/server/src/producer/index.js
+++ web-app/server/src/producer/index.js
@@ -1,89 +1,69 @@
-var Kafka = require('node-rdkafka');
+const Kafka = require("node-rdkafka");
 
-var producer = new Kafka.Producer(
-  { 
-    //'debug' : 'all',
-    'metadata.broker.list': `${process.env.KAFKA_HOST}:${process.env.KAFKA_SERVER_PORT}` 
-  }, 
-  {}, 
-  { 
-    topic: `${process.env.TOPIC}`,
-  }
+const producer = new Kafka.Producer(
+    {
+      "client.id": "tasks-producer-1",
+      "metadata.broker.list": `${process.env.KAFKA_HOST}:${process.env.KAFKA_SERVER_PORT}`
+    }
 );
 
- producer.on('event.log', function(log) {
-     console.log(JSON.stringify(log));
- });
-
-producer.on('event.error', function(err) {
-    console.error('Error from producer');
-    console.error(err);
-});
-
-producer.on('disconnected', function(arg) {
-    console.log('producer disconnected. ' + JSON.stringify(arg));
-});
-
-const client = Kafka.AdminClient.create({
-  'client.id':            `${process.env.KAFKA_ADMIN_CLIENT_ID}`,
-  'bootstrap.servers' :   `${process.env.KAFKA_HOST}:${process.env.KAFKA_SERVER_PORT}`,
-  'metadata.broker.list': `${process.env.KAFKA_HOST}:${process.env.KAFKA_SERVER_PORT}`
-});
+producer.setPollInterval(100);
 
 producer
-  .on('ready', function(i, metadata) {
-    console.log("Producer ready");
-    var is_topic_tasks_created = false
-    metadata.topics.forEach((topic) => {
-      if (topic.name === `${process.env.DB_TASKS_TABLE_NAME}`) {
-        is_topic_tasks_created = true;
-        return;
-      }
-    })
-    if (is_topic_tasks_created === true) {
-      console.log(`Topic '${process.env.DB_TASKS_TABLE_NAME}' already exists`)
-    } else {
-      console.log(`Topic '${process.env.DB_TASKS_TABLE_NAME}' not found`)
-      
-      client.createTopic({
-        topic: `${process.env.DB_TASKS_TABLE_NAME}`,
-        num_partitions: 1,
-        replication_factor: 1
-      }, (res) => {
-        if (res !== undefined) console.log(res)
-        else { 
-          console.log(`Topic '${process.env.DB_TASKS_TABLE_NAME}' was created`)
-          client.disconnect();
+    .on("event.log", function(log) {
+      console.log(log);
+    });
+
+producer
+    .on("event.error", function(err) {
+      console.error("Error from producer");
+      console.error(err);
+    });
+
+producer
+    .on("disconnected", function(arg) {
+      console.log("producer disconnected. " + JSON.stringify(arg));
+    });
+
+const client = Kafka.AdminClient.create({
+  "client.id": `${process.env.KAFKA_ADMIN_CLIENT_ID}`,
+  "bootstrap.servers":
+      `${process.env.KAFKA_HOST}:${process.env.KAFKA_SERVER_PORT}`,
+  "metadata.broker.list":
+      `${process.env.KAFKA_HOST}:${process.env.KAFKA_SERVER_PORT}`
+});
+
+producer.connect()
+    .on("ready", (i, metadata) => {
+      let isTopicTasksCreated = false;
+      metadata.topics.forEach((topic) => {
+        if (topic.name === process.env.KAFKA_TOPIC_NAME) {
+          isTopicTasksCreated = true;
         }
       });
-    }
-  })
-  .on('event.error', function(err) {
-    console.log(err);
-  })
-
-  function connect() {
-      return new Promise((resolve, reject) => {
-          producer.connect({}, (err) => {
-          if (err) {
-              console.log('Error connecting to Broker');
-              console.log(err);
-              reject(err);
-            } else {
-              resolve(producer);
-            }
-          });
-      });
-  }
-
-function connectProducer(producer) {
-     connect()
-      .then(() => {})
-      .catch(err => {
-        setTimeout(() => connectProducer(producer), 2000);
+      if (isTopicTasksCreated === true) {
+        console.debug(`Topic '${process.env.DB_TASKS_TABLE_NAME}' already exists`);
+      } else {
+        console.debug(`Topic '${process.env.DB_TASKS_TABLE_NAME}' not found`);
+        console.debug(`Creating topic '${process.env.DB_TASKS_TABLE_NAME}'`);
+        client.createTopic({
+          topic: `${process.env.DB_TASKS_TABLE_NAME}`,
+          num_partitions: 1,
+          replication_factor: 1
+        }, function(res) {
+          if (res !== undefined) {
+            console.debug(res);
+          } else {
+            console.debug(`Topic '${process.env.DB_TASKS_TABLE_NAME}' 
+                           was created`);
+            client.disconnect();
+          }
+        });
+      }
+    })
+    .on("event.error", function(err) {
+      console.log("err");
+      console.log(err);
     });
-};
-
-connectProducer(producer);
 
 module.exports = producer;
