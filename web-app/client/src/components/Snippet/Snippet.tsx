e0194dc686d5d37db40ef944a0728a3ce2238223
Chizhov Anton <zr9ihi@gmail.com>
2021-11-25 20:49:47 +0300

    Add: Added documentation (WIP)

--- web-app/client/src/components/Snippet/Snippet.tsx
+++ web-app/client/src/components/Snippet/Snippet.tsx
@@ -1,49 +1,81 @@
-/* eslint-disable no-console,indent,function-paren-newline */
-import React, { useState, useContext } from "react";
-import { Container } from "react-bootstrap";
-
+/* eslint-disable no-console */
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { coloredDepedency } from "../../types";
 import "./Snippet.scss";
-import Toggle from "../Toggle/Toggle";
-import { dependency } from "../../types";
-import Table from "./Table";
-import { TaskContext } from "../TaskContext/TaskContext";
+
+import { serverURL } from "../../APIFunctions";
 
 interface Props {
-  selectedDependency: dependency | null;
-  className?: string;
+  taskId: string;
+  selectedDependency: coloredDepedency | undefined;
 }
 
-const Snippet: React.FC<Props> = ({ selectedDependency, className = "" }) => {
-  const { table } = useContext(TaskContext)!;
-  const [isNonSelectedPartShown, setIsNonSelectedPartShown] = useState(true);
-  const header = table[0];
+/* eslint-disable prefer-template */
+/* eslint-disable no-cond-assign */
+/* eslint-disable prefer-destructuring */
 
-  const getSelectedAttributeColumns = () => {
-    if (!selectedDependency) {
-      return [];
-    }
-    const lhs = selectedDependency.lhs.map((attr) => header.indexOf(attr.name));
-    const rhs = header.indexOf(selectedDependency.rhs.name);
-    return [...lhs, rhs];
-  };
+const MAX_LENGTH = 100;
+
+// eslint-disable-next-line max-len
+function getSelectedIndices(
+  dep: coloredDepedency | undefined,
+  header: string[]
+): Map<number, string> {
+  const selectedIndices = new Map<number, string>();
+  if (dep === undefined) {
+    return selectedIndices;
+  }
+  dep.lhs.forEach((lhs) =>
+    selectedIndices.set(header.indexOf(lhs.name), lhs.color));
+  selectedIndices.set(header.indexOf(dep.rhs.name), dep.rhs.color);
+  return selectedIndices;
+}
+
+const Snippet: React.FC<Props> = ({ taskId, selectedDependency }) => {
+  const [table, setTable] = useState<string[][]>([[]]);
+  const selectedIndices: Map<number, string> = getSelectedIndices(
+    selectedDependency,
+    table[0]
+  );
+
+  useEffect(() => {
+    axios
+      .get(`${serverURL}/getSnippet/${taskId}`)
+      .then((res) => setTable(res.data));
+  }, []);
 
   return (
-    <Container fluid className={`flex-grow-1 ${className}`}>
-      <Toggle
-        variant="dark"
-        onClick={() => setIsNonSelectedPartShown(!isNonSelectedPartShown)}
-        toggleCondition={isNonSelectedPartShown}
-        isEnabled={!!selectedDependency}
-        className="my-2"
-      >
-        Snow non-selected
-      </Toggle>
-      <Table
-        colorizedColumns={getSelectedAttributeColumns()}
-        showUncolorizedColumns={isNonSelectedPartShown || !selectedDependency}
-        data={table}
-      />
-    </Container>
+    <table>
+      {table === undefined ? (
+        <></>
+      ) : (
+        table
+          .filter((val, idx) => idx < MAX_LENGTH)
+          .map((value, index) => (
+            <tr
+              // eslint-disable-next-line react/no-array-index-key
+              key={index}
+            >
+              {value
+                .filter((cell, idx) => idx !== value.length - 1 || cell !== "")
+                .map((cell, idx) => (
+                  <td
+                    // eslint-disable-next-line react/no-array-index-key
+                    key={idx}
+                    style={
+                      selectedIndices.get(idx) !== undefined
+                        ? { backgroundColor: selectedIndices.get(idx) + "90" }
+                        : { backgroundColor: "#ffffff" }
+                    }
+                  >
+                    {cell}
+                  </td>
+                ))}
+            </tr>
+          ))
+      )}
+    </table>
   );
 };
 
