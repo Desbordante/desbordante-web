a48f470e8b06e8a29fbdc23654f0fee39c03f2c3
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-02-04 00:39:46 +0300

    Delete: unused files

--- web-app/client/src/mocks.js
+++
@@ -1,9 +0,0 @@
-window.HTMLElement.prototype.scrollIntoView = function() {};
-
-jest.mock("react-chartjs-2", () => ({
-  Doughnut: () => null,
-}));
-
-Object.defineProperty(HTMLMediaElement.prototype, "muted", {
-  set: () => {},
-});
