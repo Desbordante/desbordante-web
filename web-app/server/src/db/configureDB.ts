2d7fe5a06b70134d9df829625328defcc33d2520
vs9h <zr9ihi@gmail.com>
2022-02-05 20:34:08 +0300

    WIP: Moving from REST API to GraphQL

--- web-app/server/src/db/configureDB.ts
+++ web-app/server/src/db/configureDB.ts
@@ -1,24 +1,12 @@
 import express from "express";
-import { Pool } from "pg";
 
 import createPool from "./createPool"
-import createTable from "./createTable"
-import dropTableTasks from "./dropTable"
 import createDB from "./createDB"
 
 async function configureDB(app: express.Application) {
     return createDB()
         .then(async () => {
             return await createPool();
-        })
-        // .then(async (pool: Pool) => {
-        //     await dropTableTasks(pool);
-        //     return pool;
-        // })
-        .then(async (pool: Pool) => {
-            app.set("pool", pool);
-            await createTable(pool);
-            return pool;
         })
         .catch((err: any) => {
             console.error(err);
--- web-app/server/src/graphql/schema/TaskInfo/resolvers.ts
+++ web-app/server/src/graphql/schema/TaskInfo/resolvers.ts
@@ -1,43 +1,95 @@
-import {QueryResolvers} from "../../types/types";
-import {Pool} from "pg";
+import { ApolloError } from "apollo-server-core";
+import { CsvParserStream, parse } from "fast-csv";
+import { Row } from "@fast-csv/parse";
+import fs from "fs";
 
+import { Resolvers } from "../../types/types";
 
-const Query: QueryResolvers = {
-    taskInfo: async (obj, { id }, { pool } : { pool : Pool}) => {
-        let query = `select status, fileName, trim(algName) as algorithmName, errorPercent, progress, currentPhase
-                     from ${process.env.DB_TASKS_TABLE_NAME}
-                     where taskid = '${id}' and "status" != 'CANCELLED'`;
-        return await pool.query(query)
-            .then(({ rows }) => {
-                if (rows.length !== 1) {
-                    console.log('return0');
-                    return {
-                        __typename: "TaskNotFoundError",
-                        msg: `The task with the id ${id} does not exist.`,
-                    };
-                } else {
-                    console.log(rows[0]);
-                    const data: any = {
-                        ID: id,
-                        phaseName: rows[0].phasename,
-                        progress: rows[0].progress,
-                        currentPhase: rows[0].currentphase,
-                        msg: rows[0].status,
-                        fileName: rows[0].filename,
-                        algorithmName: rows[0].algorithmname,
-                        errorThreshold: rows[0].errorpercent
-                    };
-                    return {
-                        __typename: "BaseTaskInfo",
-                        ...data
-                    };
-                }
-            })
-            .catch(err => {
-                throw new Error(err);
-            });
+const resolvers: Resolvers = {
+    TaskInfoAnswer: {
+        __resolveType(obj, { models, logger }, info) {
+            switch (obj.info.type) {
+                 case "FDA":
+                    return "FDATaskInfo";
+                 case "CFDA":
+                     return "CFDATaskInfo";
+                 default:
+                     return null;
+            }
+        }
     },
-    TaskStatus:
+    BaseTaskConfig: {
+        table: async(parent, {}, { models, logger }) => {
+            // @ts-ignore
+            const config = await models.TaskConfig.findByPk(parent.taskID);
+            return await models.FileInfo.findByPk(config.fileID);
+        }
+    },
+    FDTaskConfig: {
+      baseConfig: async(obj, { }, { models, logger }) => {
+          logger("I'm there");
+          logger(JSON.stringify(obj));
+          // @ts-ignore
+          return models.TaskConfig.findByPk(obj.taskID);
+      }
+    },
+    Snippet: {
+      table: async(parent, {}, { models, logger }) => {
+          // @ts-ignore
+          const { fileID } = parent;
+          return await models.FileInfo.findByPk(fileID);
+      }
+    },
+
+    Query: {
+        taskInfo: async (parent, { id }, { models, logger }) => {
+            const info = await models.TaskInfo.findByPk(id);
+            logger(JSON.stringify(info));
+            const fdConfig = await models.FDTaskConfig.findByPk(id);
+            return {
+                info,
+                "config": fdConfig,
+            };
+        },
+        snippet: async (parent, { taskID, from, limit}, { models, logger }) => {
+            try {
+                const taskConfig = await models.TaskConfig.findByPk(taskID);
+                const { fileID } = taskConfig;
+
+                const fileInfo = await models.FileInfo.findByPk(fileID);
+                const { path, delimiter, hasHeader, renamedHeader } = fileInfo;
+
+                const rows: any[] = [];
+                if (hasHeader === true) {
+                    from += 1;
+                }
+                const maxRows = limit - from + 1;
+
+                return await new Promise(resolve => {
+                    const parser: CsvParserStream<Row, Row> = parse({
+                        delimiter,
+                        skipRows: from - 1,
+                        maxRows,
+                    });
+
+                    fs.createReadStream(path)
+                        .pipe(parser)
+                        .on("error", (error) => {
+                            // 400
+                        })
+                        .on("data", (row) => {
+                            rows.push(row);
+                        })
+                        .on("end", () => {
+                            // @ts-ignore
+                            resolve({ rows, "header": JSON.parse(renamedHeader), fileID });
+                        });
+                })
+            } catch (e) {
+                throw new ApolloError("INTERNAL SERVER ERROR", e);
+            }
+        }
+    }
 }
 
-export default { Query };
+export default resolvers;
