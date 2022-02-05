27c99b1b7aabdeb8ec6d627ffcf411e29e64c321
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-02-05 23:34:27 +0300

    Add: feedback form

---
+++ web-app/client/src/components/FeedbackForm/StarRatingPicker.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+interface Props {
+  rating: number;
+  onChange: (n: number) => void;
+  max: number;
+}
+
+const StarRatingPicker: React.FC<Props> = ({ max, onChange, rating }) => {
+  const [selectedMark, setSelectedMark] = useState(rating);
+  const [hover, setHover] = useState(false);
+
+  return (
+    <h3
+      className={`text-primary opacity-${hover ? "75" : "100"}`}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => {
+        setHover(false);
+        setSelectedMark(rating);
+      }}
+    >
+      {[...Array(max)].map((_, idx) => (
+        <i
+          /* eslint-disable-next-line react/no-array-index-key */
+          key={idx}
+          className={`mx-1 bi bi-star${idx < selectedMark ? "-fill" : ""}`}
+          onMouseEnter={() => setSelectedMark(idx)}
+          onClick={() => onChange(idx + 1)}
+        />
+      ))}
+    </h3>
+  );
+};
+
+export default StarRatingPicker;
