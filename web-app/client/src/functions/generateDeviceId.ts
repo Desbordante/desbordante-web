4ae2dd79d96685aa70530714d274358ec562af1c
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-02-19 19:34:01 +0300

    Add: more info about device

--- web-app/client/src/functions/generateDeviceId.ts
+++ web-app/client/src/functions/generateDeviceId.ts
@@ -1,10 +1,20 @@
-import fingerprintJS from "@fingerprintjs/fingerprintjs";
-import _ from "lodash";
+const { ClientJS } = require("clientjs");
 
-const fpPromise = fingerprintJS.load();
+const client = new ClientJS();
 
-export default async function () {
-  const fp = await fpPromise;
-  const result = _.pick(await fp.get(), ["visitorId", "components"]);
+export default function () {
+  const result = {
+    userAgent: client.getUserAgent(),
+    browser: client.getBrowser(),
+    engine: client.getEngine(),
+    os: client.getOS(),
+    osVersion: client.getOSVersion(),
+    device: client.getDevice(),
+    cpu: client.getCPU(),
+    screen: client.getScreenPrint(),
+    plugins: client.getPlugins(),
+    timeZone: client.getTimeZone(),
+    language: client.getLanguage(),
+  };
   return window.btoa(JSON.stringify(result));
 }
