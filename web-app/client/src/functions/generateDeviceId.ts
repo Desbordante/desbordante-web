73c0a950361201519470c17561819db1dadc1ec1
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-02-19 18:39:24 +0300

    Change: deviceId generation algorithm

--- web-app/client/src/functions/generateDeviceId.ts
+++ web-app/client/src/functions/generateDeviceId.ts
@@ -1,9 +1,10 @@
 import fingerprintJS from "@fingerprintjs/fingerprintjs";
+import _ from "lodash";
 
 const fpPromise = fingerprintJS.load();
 
 export default async function () {
   const fp = await fpPromise;
-  const result = await fp.get();
-  return result.visitorId;
+  const result = _.pick(await fp.get(), ["visitorId", "components"]);
+  return window.btoa(JSON.stringify(result));
 }
