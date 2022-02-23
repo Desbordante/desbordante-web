d0eca81b636f8113d2752851e13089e7a492a9c9
vs9h <zr9ihi@gmail.com>
2022-02-23 22:51:56 +0300

    Fix: Resolve merge conflicts

--- web-app/client/src/components/FileForm/CreateTaskButton.tsx
+++ web-app/client/src/components/FileForm/CreateTaskButton.tsx
@@ -2,13 +2,19 @@ import React, { useContext, useEffect } from "react";
 import { DefaultContext, useMutation } from "@apollo/client";
 import { useHistory } from "react-router-dom";
 
-import { CREATE_FD_TASK } from "../../graphql/operations/mutations/createFDTask";
-import { CHOOSE_FD_TASK } from "../../graphql/operations/mutations/chooseFDTask";
+import { CREATE_TASK_WITH_UPLOADING_DATASET } from "../../graphql/operations/mutations/createTask";
+import { CREATE_TASK_WITH_CHOOSING_DATASET } from "../../graphql/operations/mutations/chooseTask";
 import Button from "../Button/Button";
 import { FileFormContext } from "../FileFormContext";
 import { ErrorContext } from "../ErrorContext";
-import { FDTaskProps, FileProps } from "../../../__generated__/globalTypes";
 import { BuiltinDataset, isBuiltinDataset } from "../../types/dataset";
+import { FileProps, IntersectionTaskProps, PrimitiveType } from "../../types/globalTypes";
+import {
+  createTaskWithDatasetUploading, createTaskWithDatasetUploadingVariables
+} from "../../graphql/operations/mutations/__generated__/createTaskWithDatasetUploading";
+import {
+  createTaskWithDatasetChoosing, createTaskWithDatasetChoosingVariables
+} from "../../graphql/operations/mutations/__generated__/createTaskWithDatasetChoosing";
 
 const CreateTaskButton = () => {
   const { isValid, dataset, fileProps, algorithmProps, setFileUploadProgress } =
@@ -16,24 +22,19 @@ const CreateTaskButton = () => {
   const { showError } = useContext(ErrorContext)!;
 
   const [
-    createTask,
-    {
-      data: createTaskData,
-      loading: createTaskLoading,
-      error: createTaskError,
-    },
-  ] = useMutation(CREATE_FD_TASK);
+      createTask,
+    { data: createTaskData, loading: createTaskLoading, error: createTaskError }
+  ] = useMutation<createTaskWithDatasetUploading, createTaskWithDatasetUploadingVariables>(
+      CREATE_TASK_WITH_UPLOADING_DATASET);
   const [
-    chooseTask,
-    {
-      data: chooseTaskData,
-      loading: chooseTaskLoading,
-      error: chooseTaskError,
-    },
-  ] = useMutation(CHOOSE_FD_TASK);
+      chooseTask,
+    { data: chooseTaskData, loading: chooseTaskLoading, error: chooseTaskError },
+  ] = useMutation<createTaskWithDatasetChoosing, createTaskWithDatasetChoosingVariables>(
+      CREATE_TASK_WITH_CHOOSING_DATASET);
   const data =
-    (createTaskData && createTaskData.createFDTask) ||
-    (chooseTaskData && chooseTaskData.chooseFDTask);
+    (createTaskData && createTaskData.createTaskWithDatasetUploading) ||
+    (chooseTaskData && chooseTaskData.createTaskWithDatasetChoosing);
+
   const loading = createTaskLoading || chooseTaskLoading;
   const error = createTaskError || chooseTaskError;
 
@@ -51,8 +52,9 @@ const CreateTaskButton = () => {
 
   const submit = () => {
     if (isValid) {
-      const props: FDTaskProps = {
+      const props: IntersectionTaskProps = {
         algorithmName: algorithmProps.algorithm!.name,
+        type: PrimitiveType.FD,
         errorThreshold: algorithmProps.algorithm!.properties.hasErrorThreshold
           ? +algorithmProps.errorThreshold!
           : 0,
@@ -82,11 +84,7 @@ const CreateTaskButton = () => {
         });
       } else {
         createTask({
-          variables: {
-            props,
-            datasetProps,
-            table: dataset as File,
-          },
+          variables: { props, datasetProps, table: dataset as File },
           context,
         });
       }
