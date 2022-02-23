0ed86b25c1d7c932f8adec303df5b1c68c2aa818
vs9h <zr9ihi@gmail.com>
2022-02-23 23:00:14 +0300

    Add & Fix: Added graphql resolvers & server deps update & linter fixes

--- web-app/server/src/graphql/configureGraphQL.ts
+++ web-app/server/src/graphql/configureGraphQL.ts
@@ -1,31 +1,88 @@
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, AuthenticationError } from "apollo-server-express";
 import { Application } from "express";
-import { graphqlHTTP } from 'express-graphql';
-import { Sequelize } from 'sequelize';
+import { graphqlHTTP } from "express-graphql";
+import { Sequelize } from "sequelize";
+import { ModelsType } from "../db/models";
+import { Device, DeviceInfoInstance } from "../db/models/Device";
+import { AccessTokenInstance } from "../db/models/Session";
 
-import schema from './schema/schema'
+import schema from "./schema/schema";
+import { Context } from "./types/context";
 
-const configureGraphQL = async (app: Application, { models }: Sequelize) => {
-    const logger = (msg:string) => { console.log(msg); }
-
+const configureGraphQL = async (app: Application, sequelize: Sequelize) => {
+    const models = sequelize.models as ModelsType;
+    const logger = console.log;
     const graphqlServer = new ApolloServer({
         schema,
-        context : { models, logger },
-        introspection: true
+        context: async ({ req }) => {
+            const requestID = req.headers["x-request-id"];
+            if (typeof requestID !== "string") {
+                throw new AuthenticationError("requestID wasn't provided");
+            }
+
+            const deviceInfoBase64 = req.headers["x-device"];
+            if (typeof deviceInfoBase64 !== "string") {
+                throw new AuthenticationError("device info wasn't provided");
+            }
+
+            const deviceInfo: DeviceInfoInstance = JSON.parse(
+                Buffer.from(deviceInfoBase64, "base64").toString()
+            );
+
+            let device = await Device.findByPk(deviceInfo.deviceID);
+            if (!device) {
+                device = await Device.addDevice(deviceInfo);
+            } else {
+                if (!device.isEqualTo(deviceInfo)) {
+                    logger(`FATAL ERROR: Received device with duplicate deviceID = ${device.deviceID}`,
+                            JSON.stringify(device), JSON.stringify(deviceInfo));
+                }
+            }
+            const tokenPayload = req.user as AccessTokenInstance | null;
+            if (tokenPayload) {
+                const { userID, deviceID, sessionID } = tokenPayload;
+                if (deviceID !== device.deviceID) {
+                    throw new AuthenticationError(
+                        `Got incorrect deviceID (In access token = ${deviceID}`
+                        + `and in device-info header = ${device.deviceID})`);
+                }
+                const session = await models.Session.findByPk(sessionID);
+                if (!session) {
+                    throw new AuthenticationError("Session not found");
+                }
+                if (session.deviceID !== deviceID) {
+                    await session.update({ status: "INVALID" });
+                    throw new AuthenticationError("Session has another deviceID");
+                }
+                if (session.userID != userID) {
+                    throw new AuthenticationError("Received incorrect userID");
+                }
+            }
+            if (process.env.KAFKA_TOPIC_NAME === undefined) {
+                throw new Error("CANNOT GET TOPIC NAME FROM .env");
+            }
+            const topicNames = {
+                DepAlgs: process.env.KAFKA_TOPIC_NAME,
+            };
+            const contextObject: Context =
+                { models, logger, device, sessionInfo: tokenPayload, topicNames };
+            return contextObject;
+        },
+        introspection: true,
     });
     app.get(
-        '/graphql',
+        "/graphql",
         graphqlHTTP({
             schema,
-            graphiql: true
+            graphiql: true,
         })
     );
     await graphqlServer.start();
 
     graphqlServer.applyMiddleware({
         app,
-        path: '/graphql'
+        path: "/graphql",
     });
-}
+};
 
 export = configureGraphQL;
---
+++ web-app/server/src/graphql/schema/UserResolvers/resolvers.ts
@@ -0,0 +1,148 @@
+import { ApolloError, ForbiddenError, UserInputError } from "apollo-server-core";
+import { AuthenticationError } from "apollo-server-express";
+import jwt from "jsonwebtoken";
+import { PermissionEnum, RoleEnum } from "../../../db/models/permissionsConfig";
+import { Role } from "../../../db/models/Role";
+import { RefreshTokenInstance } from "../../../db/models/Session";
+import { Resolvers } from "../../types/types";
+
+export const UserResolvers : Resolvers = {
+    Query: {
+        getAnonymousPermissions: (parent, obj, { models, logger }) => {
+            return Role.getPermissionForRole(RoleEnum.ANONYMOUS).map(permissionEnum => permissionEnum.toString());
+        },
+        user: async(parent, { userID }, { models, logger, sessionInfo }) => {
+            if (!sessionInfo) {
+                throw new AuthenticationError("User must be authorized");
+            }
+            if (sessionInfo.permissions.includes(PermissionEnum.VIEW_ADMIN_INFO) || sessionInfo.userID === userID) {
+                return await models.User.findOne({ where: { userID } })
+                    .then(res => {
+                        if (res) {
+                            return res;
+                        } else {
+                            throw new UserInputError("Invalid user ID", { userID });
+                        }
+                    })
+                    .catch((e) => e);
+            } else {
+                throw new ForbiddenError("User doesn't have permissions");
+            }
+        },
+    },
+    Mutation: {
+        // @ts-ignore
+        createFeedback: async (parent, args, { models, logger, sessionInfo }) => {
+            const userID = sessionInfo ? sessionInfo.userID : null;
+            return await models.Feedback.create({ userID, ...args });
+        },
+        logIn: async (parent, { email, pwdHash }, { models, logger, device, sessionInfo }) => {
+            if (sessionInfo) {
+                throw new AuthenticationError("You are already logged");
+            }
+            const user = await models.User.findOne({ where: { email, pwdHash } });
+            if (!user) {
+                throw new UserInputError("Incorrect login or password");
+            }
+            const session = await user.createSession(device.deviceID);
+            return await session.issueTokenPair();
+        },
+        logOut: async (parent, { allSessions }, { models, logger, device, sessionInfo }) => {
+            if (!sessionInfo) {
+                throw new UserInputError("Session information wasn't provided");
+            }
+            const [affectedRows] = await models.Session.update({ status: "INVALID" },
+                allSessions ? { where: { userID: sessionInfo.userID } } : { where: { sessionID: sessionInfo.sessionID } });
+            if (affectedRows >= 1) {
+                return `Successfully updated ${affectedRows} sessions`;
+            } else {
+                throw new ApolloError("Session wasn't updated");
+            }
+        },
+        createUser: async (parent, { props }, { models, logger, sessionInfo, device }) => {
+            if (sessionInfo) {
+                throw new AuthenticationError("User already logged in");
+            }
+            const user = await models.User.findOne({ where: { email: props.email } });
+            if (user) {
+                throw new UserInputError(`Email ${props.email} already used`);
+            }
+
+            const newUser = await models.User.create({ ...props, accountStatus: "EMAIL VERIFICATION" });
+
+            const code = await models.Code.createEmailVerificationCode(newUser.userID, device.deviceID);
+
+            logger(`New account created, sent verification code = ${code.value}`);
+            return {
+                message: "Verification code was sent to email",
+                userID: newUser.userID,
+            };
+        },
+        approveUserEmail: async (parent, { codeValue, userID }, { models, logger, device, sessionInfo }) => {
+            if (sessionInfo) {
+                throw new AuthenticationError("User mustn't have sessionInfo");
+            }
+            const user = await models.User.findByPk(userID);
+
+            if (!user) {
+                throw new UserInputError("Incorrect userID was provided");
+            }
+            if (user.accountStatus !== "EMAIL VERIFICATION") {
+                throw new UserInputError("User has incorrect account status");
+            }
+            const code = await models.Code.findOne({ where: { userID, type: "EMAIL VERIFICATION" } });
+            if (!code) {
+                throw new UserInputError("User hasn't email verification codes");
+            }
+            if (code.deviceID !== device.deviceID) {
+                await code.destroy();
+                throw new UserInputError("Request sent from another device, temporary code destroyed");
+            }
+
+            if (code.expiringDate < new Date(new Date().toUTCString())) {
+                await code.destroy();
+                throw new UserInputError("Code was expired");
+            }
+
+            if (code.value !== codeValue) {
+                await code.destroy();
+                throw new UserInputError("Received incorrect code value, temporary code was destroyed");
+            } else {
+                await user.update({ accountStatus: "EMAIL VERIFIED" });
+                await user.addRole(RoleEnum.USER);
+
+                const session = await user.createSession(device.deviceID);
+                return session.issueTokenPair();
+            }
+        },
+        refresh: async (parent, { refreshToken }, { models, logger, device }) => {
+            let decoded: RefreshTokenInstance;
+            try {
+                decoded = jwt.verify(refreshToken, process.env.SECRET_KEY!) as RefreshTokenInstance;
+            } catch (e) {
+                throw new AuthenticationError("INVALID REFRESH TOKEN WAS PROVIDED");
+            }
+            const { userID, sessionID, deviceID, iat } = decoded;
+            const session = await models.Session.findByPk(sessionID,
+                { attributes: ["refreshTokenIat", "status", "deviceID", "userID"] });
+
+            if (!session) {
+                throw new AuthenticationError("Invalid sessionID was provided");
+            }
+            if (userID != session.userID) {
+                throw new AuthenticationError("Session have another user");
+            }
+            if (session.refreshTokenIat !== iat) {
+                throw new AuthenticationError("Received expired refresh token");
+            }
+            if (session.status !== "VALID") {
+                throw new AuthenticationError("Using invalid session");
+            }
+            if (session.deviceID !== device.deviceID
+                || session.deviceID !== deviceID) {
+                throw new AuthenticationError("User's device changed");
+            }
+            return session.issueTokenPair();
+        },
+    },
+};
