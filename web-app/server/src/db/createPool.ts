e6899d4b931165b150d7c0e78d5e04f367838acb
vs9h <zr9ihi@gmail.com>
2022-02-23 18:56:25 +0300

    Fix: Fixed warnings, highlighted by eslint

--- web-app/server/src/db/createPool.ts
+++ web-app/server/src/db/createPool.ts
@@ -1,18 +1,14 @@
 import { Pool } from "pg";
 
 async function createPool() {
-  // pools uses environment variables
-  // for connection information
   const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT),
+    port: parseInt(process.env.DB_PORT!),
   });
 
-  // the pool will emit an error on behalf of any idle clients
-  // it contains if a backend error or network partition happens
   pool.on("error", (err) => {
     console.error("Unexpected error on idle client", err);
     throw err;
