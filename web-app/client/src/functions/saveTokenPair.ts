654533e73bef370e29b61c0b33cc0864a6f72dde
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-02-25 19:46:28 +0300

    Add: sign out functionality

--- web-app/client/src/functions/saveTokenPair.ts
+++
@@ -1,9 +0,0 @@
-import cookie from "cookie";
-import { TokenPair } from "../types/types";
-
-export default function saveTokenPair(tokenPair: TokenPair) {
-  document.cookie = cookie.serialize("accessToken", tokenPair.accessToken, {
-    maxAge: 15 * 60,
-  });
-  document.cookie = cookie.serialize("refreshToken", tokenPair.refreshToken);
-}
