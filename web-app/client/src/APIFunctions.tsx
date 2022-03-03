a50352128bb1f259381715d473fa479071cf0fae
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-03-03 15:03:35 +0300

    Add: server protocol env variable

--- web-app/client/src/APIFunctions.tsx
+++ web-app/client/src/APIFunctions.tsx
@@ -1,2 +1,2 @@
-export const serverURL = `http://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`;
+export const serverURL = `${process.env.REACT_APP_SERVER_PROTOCOL}://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`;
 export const graphQLEndpoint = `${serverURL}/graphql`;
