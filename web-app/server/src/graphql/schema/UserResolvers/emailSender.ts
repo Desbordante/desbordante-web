3197758ba9a2cb2df1fdcca1c339af90a6c1187c
vs9h <zr9ihi@gmail.com>
2022-03-05 14:21:03 +0300

    Add: Added email sending for verification

--- web-app/server/src/graphql/schema/UserResolvers/emailSender.ts
+++ web-app/server/src/graphql/schema/UserResolvers/emailSender.ts
@@ -1,5 +1,21 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    from: "",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: process.env.NODEMAILER_EMAIL,
+        pass: process.env.NODEMAILER_PWD,
+    },
 });
+
+export const sendVerificationCode = async (code: number, userEmail: string) => {
+    return await transporter.sendMail({
+        from: "Desbordante enjoyer <sashasmolenfab@gmail.com>",
+        to: userEmail,
+        subject: "Email verification code",
+        text: `Your verification code is: ${code}.`,
+    });
+};
