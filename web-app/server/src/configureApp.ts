8b8274c35b3d8ce5e906505e64d7f44d3bdec0b3
vs9h <zr9ihi@gmail.com>
2022-03-05 19:04:22 +0300

    Add: Added built in dataset & new cfd task example

--- web-app/server/src/configureApp.ts
+++ web-app/server/src/configureApp.ts
@@ -93,99 +93,23 @@ async function createAccountWithLongLiveRefreshToken(roles: RoleType[]) {
 }
 
 // Function for client (test UI)
-async function createCfdTask() {
-    const jsonStr = `[
-{ "l": ["First"], "lp": ["1"], "r": "Second", "rp": "2" },
-{ "l": ["Third"], "lp": ["1"], "r": "First", "rp": "1" },
-{ "l": ["Third"], "lp": ["3"], "r": "First", "rp": "1" },
-{ "l": ["Second"], "lp": ["4"], "r": "First", "rp": "3" },
-{ "l": ["Third"], "lp": ["4"], "r": "First", "rp": "3" },
-{ "l": ["Third"], "lp": ["5"], "r": "First", "rp": "3" },
-{ "l": ["Third"], "lp": ["8"], "r": "First", "rp": "5" },
-{ "l": ["Second"], "lp": ["4"], "r": "Third", "rp": "5" },
-{ "l": ["Third"], "lp": ["5"], "r": "Second", "rp": "4" },
-{ "l": ["Third"], "lp": ["2"], "r": "Second", "rp": "3" },
-{ "l": ["Third"], "lp": ["4"], "r": "Second", "rp": "3" },
-{ "l": ["Third"], "lp": ["8"], "r": "Second", "rp": "3" },
-{ "l": ["Third"], "lp": ["1"], "r": "Second", "rp": "2" },
-{ "l": ["Third"], "lp": ["3"], "r": "Second", "rp": "2" },
-{ "l": ["Third"], "lp": ["7"], "r": "Second", "rp": "2" },
-{ "l": ["First","Third"], "lp": ["_","_"], "r": "Second", "rp": "_" },
-{ "l": ["Second","Third"], "lp": ["_","8"], "r": "First", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["_","8"], "r": "Second", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["_","7"], "r": "Second", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["_","5"], "r": "Second", "rp": "_" },
-{ "l": ["Second","Third"], "lp": ["_","4"], "r": "First", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["_","4"], "r": "Second", "rp": "_" },
-{ "l": ["Second","Third"], "lp": ["_","3"], "r": "First", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["_","3"], "r": "Second", "rp": "_" },
-{ "l": ["Second","Third"], "lp": ["_","2"], "r": "First", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["_","2"], "r": "Second", "rp": "_" },
-{ "l": ["Second","Third"], "lp": ["_","1"], "r": "First", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["_","1"], "r": "Second", "rp": "_" },
-{ "l": ["First","Second"], "lp": ["_","3"], "r": "Third", "rp": "_" },
-{ "l": ["First","Second"], "lp": ["_","4"], "r": "Third", "rp": "_" },
-{ "l": ["First","Second"], "lp": ["5","_"], "r": "Third", "rp": "_" },
-{ "l": ["First","Second"], "lp": ["5","2"], "r": "Third", "rp": "6" },
-{ "l": ["First","Second"], "lp": ["5","3"], "r": "Third", "rp": "7" },
-{ "l": ["First","Second"], "lp": ["3","3"], "r": "Third", "rp": "4" },
-{ "l": ["First","Second"], "lp": ["2","_"], "r": "Third", "rp": "_" },
-{ "l": ["First","Third"], "lp": ["2","_"], "r": "Second", "rp": "_" },
-{ "l": ["First","Second"], "lp": ["2","2"], "r": "Third", "rp": "6" },
-{ "l": ["First","Second"], "lp": ["2","3"], "r": "Third", "rp": "2" }
- ]
-`;
+async function createCfdTask(fileName: string, cfdsStr: string,
+                             pieChartDataWithoutPatternsStr: string,
+                             pieChartDataWithPatternsStr: string,
+                             maxLHS = -1, minConfidence = 1, minSupport = 1) {
     type cfd = { l: [string], lp:[string], r: string, rp: string }
-    const json: { cfds: [cfd] } = JSON.parse(jsonStr);
+    const json: { cfds: [cfd] } = JSON.parse(cfdsStr);
 
-    const pieChartDataJsonStr = `
-    { "lhs":[{ "id": 0, "value":10.000000 },
-{ "id": 1, "value":9.000000 },
-{ "id": 2, "value":19.000000 }
-], "rhs":[{ "id": 0, "value":8.500000 },
-{ "id": 1, "value":12.500000 },
-{ "id": 2, "value":5.500000 }
-] }`;
     type WithoutPatternRow = { id: number, value: number };
-    const pieChartDataWithoutPatternsJson: { lhs: [WithoutPatternRow], rhs: [WithoutPatternRow] } = JSON.parse(pieChartDataJsonStr);
+    const pieChartDataWithoutPatternsJson:
+        { lhs: [WithoutPatternRow], rhs: [WithoutPatternRow] }
+        = JSON.parse(pieChartDataWithoutPatternsStr);
 
-    const pieChartDataWithPatternsJsonStr = `
-    { "lhs":[{ "id": 0, "pattern":"1", "value":1.000000 },
-{ "id": 0, "pattern":"2", "value":2.000000 },
-{ "id": 0, "pattern":"3", "value":0.500000 },
-{ "id": 0, "pattern":"5", "value":1.500000 },
-{ "id": 0, "pattern":"_", "value":5.000000 },
-{ "id": 1, "pattern":"2", "value":1.000000 },
-{ "id": 1, "pattern":"3", "value":2.000000 },
-{ "id": 1, "pattern":"4", "value":2.500000 },
-{ "id": 1, "pattern":"_", "value":3.500000 },
-{ "id": 2, "pattern":"1", "value":3.000000 },
-{ "id": 2, "pattern":"2", "value":2.000000 },
-{ "id": 2, "pattern":"3", "value":3.000000 },
-{ "id": 2, "pattern":"4", "value":3.000000 },
-{ "id": 2, "pattern":"5", "value":2.500000 },
-{ "id": 2, "pattern":"7", "value":1.500000 },
-{ "id": 2, "pattern":"8", "value":3.000000 },
-{ "id": 2, "pattern":"_", "value":1.000000 }
-], "rhs":[{ "id": 0, "pattern":"1", "value":2.000000 },
-{ "id": 0, "pattern":"3", "value":3.000000 },
-{ "id": 0, "pattern":"5", "value":1.000000 },
-{ "id": 0, "pattern":"_", "value":2.500000 },
-{ "id": 1, "pattern":"2", "value":4.000000 },
-{ "id": 1, "pattern":"3", "value":3.000000 },
-{ "id": 1, "pattern":"4", "value":1.000000 },
-{ "id": 1, "pattern":"_", "value":4.500000 },
-{ "id": 2, "pattern":"2", "value":0.500000 },
-{ "id": 2, "pattern":"4", "value":0.500000 },
-{ "id": 2, "pattern":"5", "value":1.000000 },
-{ "id": 2, "pattern":"7", "value":1.000000 },
-{ "id": 2, "pattern":"8", "value":0.500000 },
-{ "id": 2, "pattern":"_", "value":2.000000 }
-] }`;
     type WithPatternRow = { id: number, value: number, pattern: string };
-    const pieChartDataWithPatternsJson: { lhs: [WithPatternRow], rhs: [WithPatternRow] } = JSON.parse(pieChartDataWithPatternsJsonStr);
+    const pieChartDataWithPatternsJson:
+        { lhs: [WithPatternRow], rhs: [WithPatternRow] }
+        = JSON.parse(pieChartDataWithPatternsStr);
 
-    const fileName = "TestLong.csv";
     const file = await FileInfo.findOne({ where: { fileName } });
     if (!file) {
         throw new UserInputError("File not found", { fileName });
@@ -194,9 +118,9 @@ async function createCfdTask() {
         algorithmName: "CTane",
         // @ts-ignore
         type: "CFD",
-        maxLHS: -1,
-        minConfidence: 1,
-        minSupport: 1,
+        maxLHS,
+        minConfidence,
+        minSupport,
     };
     const taskInfo = await TaskInfo.saveToDBIfPropsValid(props, file.ID, null);
 
@@ -211,7 +135,214 @@ async function createCfdTask() {
     await res.update({ CFDs: JSON.stringify(json) });
     await res.update({ withPatterns: JSON.stringify(pieChartDataWithPatternsJson) });
     await res.update({ withoutPatterns: JSON.stringify(pieChartDataWithoutPatternsJson) });
-    console.log(`Created task cfd with id = ${taskInfo.taskID}`);
+    return `Created task CFD with id = ${taskInfo.taskID} (dataset ${fileName}).`;
+}
+
+async function createBuiltInCfdTasks() {
+    const testlong = await createCfdTask(
+        "TestLong.csv",
+        `[
+    { "l": ["First"], "lp": ["1"], "r": "Second", "rp": "2" },
+    { "l": ["Third"], "lp": ["1"], "r": "First", "rp": "1" },
+    { "l": ["Third"], "lp": ["3"], "r": "First", "rp": "1" },
+    { "l": ["Second"], "lp": ["4"], "r": "First", "rp": "3" },
+    { "l": ["Third"], "lp": ["4"], "r": "First", "rp": "3" },
+    { "l": ["Third"], "lp": ["5"], "r": "First", "rp": "3" },
+    { "l": ["Third"], "lp": ["8"], "r": "First", "rp": "5" },
+    { "l": ["Second"], "lp": ["4"], "r": "Third", "rp": "5" },
+    { "l": ["Third"], "lp": ["5"], "r": "Second", "rp": "4" },
+    { "l": ["Third"], "lp": ["2"], "r": "Second", "rp": "3" },
+    { "l": ["Third"], "lp": ["4"], "r": "Second", "rp": "3" },
+    { "l": ["Third"], "lp": ["8"], "r": "Second", "rp": "3" },
+    { "l": ["Third"], "lp": ["1"], "r": "Second", "rp": "2" },
+    { "l": ["Third"], "lp": ["3"], "r": "Second", "rp": "2" },
+    { "l": ["Third"], "lp": ["7"], "r": "Second", "rp": "2" },
+    { "l": ["First","Third"], "lp": ["_","_"], "r": "Second", "rp": "_" },
+    { "l": ["Second","Third"], "lp": ["_","8"], "r": "First", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["_","8"], "r": "Second", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["_","7"], "r": "Second", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["_","5"], "r": "Second", "rp": "_" },
+    { "l": ["Second","Third"], "lp": ["_","4"], "r": "First", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["_","4"], "r": "Second", "rp": "_" },
+    { "l": ["Second","Third"], "lp": ["_","3"], "r": "First", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["_","3"], "r": "Second", "rp": "_" },
+    { "l": ["Second","Third"], "lp": ["_","2"], "r": "First", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["_","2"], "r": "Second", "rp": "_" },
+    { "l": ["Second","Third"], "lp": ["_","1"], "r": "First", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["_","1"], "r": "Second", "rp": "_" },
+    { "l": ["First","Second"], "lp": ["_","3"], "r": "Third", "rp": "_" },
+    { "l": ["First","Second"], "lp": ["_","4"], "r": "Third", "rp": "_" },
+    { "l": ["First","Second"], "lp": ["5","_"], "r": "Third", "rp": "_" },
+    { "l": ["First","Second"], "lp": ["5","2"], "r": "Third", "rp": "6" },
+    { "l": ["First","Second"], "lp": ["5","3"], "r": "Third", "rp": "7" },
+    { "l": ["First","Second"], "lp": ["3","3"], "r": "Third", "rp": "4" },
+    { "l": ["First","Second"], "lp": ["2","_"], "r": "Third", "rp": "_" },
+    { "l": ["First","Third"], "lp": ["2","_"], "r": "Second", "rp": "_" },
+    { "l": ["First","Second"], "lp": ["2","2"], "r": "Third", "rp": "6" },
+    { "l": ["First","Second"], "lp": ["2","3"], "r": "Third", "rp": "2" }]`,
+        `
+    { "lhs":[{ "id": 0, "value":10.000000 },
+        { "id": 1, "value":9.000000 },
+        { "id": 2, "value":19.000000 }],
+      "rhs":[{ "id": 0, "value":8.500000 },
+        { "id": 1, "value":12.500000 },
+        { "id": 2, "value":5.500000 }]
+    }`,
+        `
+    { "lhs":[{ "id": 0, "pattern":"1", "value":1.000000 },
+    { "id": 0, "pattern":"2", "value":2.000000 },
+    { "id": 0, "pattern":"3", "value":0.500000 },
+    { "id": 0, "pattern":"5", "value":1.500000 },
+    { "id": 0, "pattern":"_", "value":5.000000 },
+    { "id": 1, "pattern":"2", "value":1.000000 },
+    { "id": 1, "pattern":"3", "value":2.000000 },
+    { "id": 1, "pattern":"4", "value":2.500000 },
+    { "id": 1, "pattern":"_", "value":3.500000 },
+    { "id": 2, "pattern":"1", "value":3.000000 },
+    { "id": 2, "pattern":"2", "value":2.000000 },
+    { "id": 2, "pattern":"3", "value":3.000000 },
+    { "id": 2, "pattern":"4", "value":3.000000 },
+    { "id": 2, "pattern":"5", "value":2.500000 },
+    { "id": 2, "pattern":"7", "value":1.500000 },
+    { "id": 2, "pattern":"8", "value":3.000000 },
+    { "id": 2, "pattern":"_", "value":1.000000 }
+    ], "rhs":[{ "id": 0, "pattern":"1", "value":2.000000 },
+    { "id": 0, "pattern":"3", "value":3.000000 },
+    { "id": 0, "pattern":"5", "value":1.000000 },
+    { "id": 0, "pattern":"_", "value":2.500000 },
+    { "id": 1, "pattern":"2", "value":4.000000 },
+    { "id": 1, "pattern":"3", "value":3.000000 },
+    { "id": 1, "pattern":"4", "value":1.000000 },
+    { "id": 1, "pattern":"_", "value":4.500000 },
+    { "id": 2, "pattern":"2", "value":0.500000 },
+    { "id": 2, "pattern":"4", "value":0.500000 },
+    { "id": 2, "pattern":"5", "value":1.000000 },
+    { "id": 2, "pattern":"7", "value":1.000000 },
+    { "id": 2, "pattern":"8", "value":0.500000 },
+    { "id": 2, "pattern":"_", "value":2.000000 }]}`);
+    const ciPublicHIghway700 = await createCfdTask("CIPublicHighway700.csv",`
+    [
+{ "l": ["CrossingID"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["SubmissionType"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["ReportBaseId"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["HwySys"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["HwyClassCD"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["HwyClassCD"], "lp": ["1"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwyClassCD"], "lp": ["0"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwyClassrdtpID"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["HwyClassrdtpID"], "lp": ["13"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwyClassrdtpID"], "lp": ["16"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwyClassrdtpID"], "lp": ["19"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwyClassrdtpID"], "lp": ["17"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwyClassrdtpID"], "lp": ["18"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["StHwy1"], "lp": ["_"], "r": "SchlBusChk", "rp": "_" },
+{ "l": ["StHwy1"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["StHwy1"], "lp": ["1"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["StHwy1"], "lp": ["2"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwySpeed"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["HwySpeed"], "lp": ["NULL"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["HwySpeedps"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["LrsRouteid"], "lp": ["_"], "r": "SchlBusChk", "rp": "_" },
+{ "l": ["LrsRouteid"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["LrsMilePost"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["Aadt"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["Aadt"], "lp": ["000260"], "r": "SchlBsCnt", "rp": "0" },
+{ "l": ["Aadt"], "lp": ["000500"], "r": "SchlBsCnt", "rp": "0" },
+{ "l": ["Aadt"], "lp": ["000050"], "r": "SchlBsCnt", "rp": "0" },
+{ "l": ["Aadt"], "lp": ["000040"], "r": "SchlBsCnt", "rp": "0" },
+{ "l": ["Aadt"], "lp": ["000150"], "r": "SchlBsCnt", "rp": "0" },
+{ "l": ["AadtYear"], "lp": ["_"], "r": "SchlBsCnt", "rp": "_" },
+{ "l": ["AadtYear"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["AadtYear"], "lp": ["1970"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["AadtYear"], "lp": ["1988"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["PctTruk"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["SchlBusChk"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["SchlBsCnt"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" },
+{ "l": ["HazmtVeh"], "lp": [""], "r": "SchlBsCnt", "rp": "0" },
+{ "l": ["SchlBsCnt"], "lp": ["0"], "r": "EmrgncySrvc", "rp": "" },
+{ "l": ["EmrgncySrvc"], "lp": [""], "r": "SchlBsCnt", "rp": "0" },
+{ "l": ["HazmtVeh"], "lp": ["_"], "r": "EmrgncySrvc", "rp": "_" }
+ ] `,
+        `{ "lhs":[{ "id": 0, "value":1.000000 },
+{ "id": 1, "value":1.000000 },
+{ "id": 2, "value":1.000000 },
+{ "id": 3, "value":1.000000 },
+{ "id": 4, "value":3.000000 },
+{ "id": 5, "value":6.000000 },
+{ "id": 6, "value":4.000000 },
+{ "id": 7, "value":2.000000 },
+{ "id": 8, "value":1.000000 },
+{ "id": 9, "value":2.000000 },
+{ "id": 10, "value":1.000000 },
+{ "id": 11, "value":6.000000 },
+{ "id": 12, "value":4.000000 },
+{ "id": 13, "value":1.000000 },
+{ "id": 14, "value":1.000000 },
+{ "id": 15, "value":2.000000 },
+{ "id": 16, "value":2.000000 },
+{ "id": 17, "value":1.000000 }
+], "rhs":[{ "id": 0, "value":0.000000 },
+{ "id": 1, "value":0.000000 },
+{ "id": 2, "value":0.000000 },
+{ "id": 3, "value":0.000000 },
+{ "id": 4, "value":0.000000 },
+{ "id": 5, "value":0.000000 },
+{ "id": 6, "value":0.000000 },
+{ "id": 7, "value":0.000000 },
+{ "id": 8, "value":0.000000 },
+{ "id": 9, "value":0.000000 },
+{ "id": 10, "value":0.000000 },
+{ "id": 11, "value":0.000000 },
+{ "id": 12, "value":0.000000 },
+{ "id": 13, "value":0.000000 },
+{ "id": 14, "value":2.000000 },
+{ "id": 15, "value":8.000000 },
+{ "id": 16, "value":0.000000 },
+{ "id": 17, "value":30.000000 }
+] }`,`{ "lhs":[{ "id": 0, "pattern":"_", "value":1.000000 },
+{ "id": 1, "pattern":"_", "value":1.000000 },
+{ "id": 2, "pattern":"_", "value":1.000000 },
+{ "id": 3, "pattern":"_", "value":1.000000 },
+{ "id": 4, "pattern":"0", "value":1.000000 },
+{ "id": 4, "pattern":"1", "value":1.000000 },
+{ "id": 4, "pattern":"_", "value":1.000000 },
+{ "id": 5, "pattern":"13", "value":1.000000 },
+{ "id": 5, "pattern":"16", "value":1.000000 },
+{ "id": 5, "pattern":"17", "value":1.000000 },
+{ "id": 5, "pattern":"18", "value":1.000000 },
+{ "id": 5, "pattern":"19", "value":1.000000 },
+{ "id": 5, "pattern":"_", "value":1.000000 },
+{ "id": 6, "pattern":"1", "value":1.000000 },
+{ "id": 6, "pattern":"2", "value":1.000000 },
+{ "id": 6, "pattern":"_", "value":2.000000 },
+{ "id": 7, "pattern":"NULL", "value":1.000000 },
+{ "id": 7, "pattern":"_", "value":1.000000 },
+{ "id": 8, "pattern":"_", "value":1.000000 },
+{ "id": 9, "pattern":"_", "value":2.000000 },
+{ "id": 10, "pattern":"_", "value":1.000000 },
+{ "id": 11, "pattern":"000040", "value":1.000000 },
+{ "id": 11, "pattern":"000050", "value":1.000000 },
+{ "id": 11, "pattern":"000150", "value":1.000000 },
+{ "id": 11, "pattern":"000260", "value":1.000000 },
+{ "id": 11, "pattern":"000500", "value":1.000000 },
+{ "id": 11, "pattern":"_", "value":1.000000 },
+{ "id": 12, "pattern":"1970", "value":1.000000 },
+{ "id": 12, "pattern":"1988", "value":1.000000 },
+{ "id": 12, "pattern":"_", "value":2.000000 },
+{ "id": 13, "pattern":"_", "value":1.000000 },
+{ "id": 14, "pattern":"_", "value":1.000000 },
+{ "id": 15, "pattern":"0", "value":1.000000 },
+{ "id": 15, "pattern":"_", "value":1.000000 },
+{ "id": 16, "pattern":"", "value":1.000000 },
+{ "id": 16, "pattern":"_", "value":1.000000 },
+{ "id": 17, "pattern":"", "value":1.000000 }
+], "rhs":[{ "id": 14, "pattern":"_", "value":2.000000 },
+{ "id": 15, "pattern":"0", "value":7.000000 },
+{ "id": 15, "pattern":"_", "value":1.000000 },
+{ "id": 17, "pattern":"", "value":13.000000 },
+{ "id": 17, "pattern":"_", "value":17.000000 }
+] }`, 2, 1, 5);
+    console.log(testlong, ciPublicHIghway700);
 }
 
 async function configureApp() {
@@ -277,7 +408,7 @@ async function configureApp() {
         await createAccountWithLongLiveRefreshToken(["ANONYMOUS", "USER", "SUPPORT", "ADMIN", "DEVELOPER"])
             .then(results => results.map(res => console.log(res)))
             .catch(e => console.error("Problems with accounts creating", e.message));
-        await createCfdTask()
+        await createBuiltInCfdTasks()
             .catch(e => console.error("Problems with task creating", e.message));
     }
 
