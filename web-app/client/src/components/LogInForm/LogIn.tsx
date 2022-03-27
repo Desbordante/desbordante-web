bd20fbb23b48a9556e21ec2e18583dc21eafbf54
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-03-27 22:55:25 +0300

    Add: restore password functionality

---
+++ web-app/client/src/components/LogInForm/LogIn.tsx
@@ -0,0 +1,134 @@
+import React, { useContext } from "react";
+import { Formik, FormikHelpers } from "formik";
+import { Form, Button } from "react-bootstrap";
+import { passwordStrength } from "check-password-strength";
+import { useMutation } from "@apollo/client";
+import * as Yup from "yup";
+
+import {
+  logIn,
+  logInVariables,
+} from "../../graphql/operations/mutations/__generated__/logIn";
+import { LOG_IN } from "../../graphql/operations/mutations/logIn";
+import hashPassword from "../../functions/hashPassword";
+import { AuthContext } from "../AuthContext";
+
+const logInSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .test("strong-enough", "Too weak!", (value?: string) =>
+      Boolean(value && passwordStrength(value).id !== 0)
+    )
+    .required("Required"),
+});
+
+interface Props {
+  onSuccess: () => void;
+  onRecovery: () => void;
+}
+
+const LogIn: React.FC<Props> = ({ onSuccess, onRecovery }) => {
+  const { applyTokens } = useContext(AuthContext)!;
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const [logIn] = useMutation<logIn, logInVariables>(LOG_IN);
+
+  const handleSubmit = async (
+    values: typeof initialValues,
+    formikHelpers: FormikHelpers<typeof initialValues>
+  ) => {
+    try {
+      const response = await logIn({
+        variables: {
+          email: values.email,
+          pwdHash: hashPassword(values.password),
+        },
+      });
+
+      if (response.data?.logIn) {
+        applyTokens(response.data.logIn);
+        onSuccess();
+      }
+    } catch (error) {
+      formikHelpers.setErrors({
+        email: "Incorrect data",
+        password: "Incorrect data",
+      });
+    }
+  };
+
+  return (
+    <>
+      <h1 className="text-center fw-bold mb-4">Log In</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={logInSchema}
+        onSubmit={handleSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                placeholder="your.email@example.com"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.email && !!errors.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                placeholder="admin1234"
+                name="password"
+                type="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.password && !!errors.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+
+            <Button
+              variant="outline-primary"
+              type="submit"
+              className="mt-2 w-100"
+              disabled={isSubmitting}
+            >
+              Log In
+            </Button>
+            <Button
+              variant="outline-dark"
+              className="mt-2 w-100"
+              onClick={onRecovery}
+            >
+              Restore My Password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default LogIn;
