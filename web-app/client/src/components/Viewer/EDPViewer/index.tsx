6931eec47888353b39b568e59457d05b2042b1d8
kirill-stupakov <kirill.stupakov.0@gmail.com>
2022-04-15 01:38:48 +0300

    Refactor: clusters logic Add: empty state message for clusters

--- web-app/client/src/components/Viewer/EDPViewer/index.tsx
+++ web-app/client/src/components/Viewer/EDPViewer/index.tsx
@@ -3,40 +3,32 @@ import { Container } from "react-bootstrap";
 
 import EDPList from "./EDPList";
 import Navigation from "../Navigation";
-import { FunctionalDependency } from "../../../types/taskInfo";
 import { TaskContext } from "../../TaskContext";
 import TableSnippet from "../TableSnippet/TableSnippet";
 import EDPClusters from "./EDPClusters";
+import { ClustersContext } from "./ClustersContext";
 
 const tabs = ["Dependencies", "Clusters", "Dataset"];
 
 const Index = () => {
-  const { taskResult } = useContext(TaskContext)!;
+  const { selectedDependency } = useContext(ClustersContext)!;
 
   const [partShown, setPartShown] = useState(0);
-  const [selectedDependency, setSelectedDependency] =
-    useState<FunctionalDependency | null>(null);
 
   return (
-    <Container fluid className="h-100 p-4 flex-grow-1 d-flex flex-column align-items-start">
+    <Container
+      fluid
+      className="h-100 p-4 flex-grow-1 d-flex flex-column align-items-start"
+    >
       <Navigation
         partShown={partShown}
         setPartShown={setPartShown}
         options={tabs}
       />
 
-      {partShown === 0 && (
-        <EDPList
-          selectedDependency={selectedDependency}
-          setSelectedDependency={setSelectedDependency}
-        />
-      )}
+      {partShown === 0 && <EDPList />}
 
-      {partShown === 1 && selectedDependency && (
-        <EDPClusters
-          selectedDependency={selectedDependency}
-        />
-      )}
+      {partShown === 1 && selectedDependency && <EDPClusters />}
 
       {partShown === 2 && (
         <TableSnippet
--- web-app/client/src/types/primitives.ts
+++ web-app/client/src/types/primitives.ts
@@ -1,4 +1,8 @@
 import {ARsFilter, FDsFilter, Pagination} from "./globalTypes";
+import {
+  getClustersPreview_taskInfo_data_result_TypoClusterTaskResult,
+  getClustersPreview_taskInfo_data_result_TypoClusterTaskResult_TypoClusters
+} from "../graphql/operations/queries/EDP/__generated__/getClustersPreview";
 
 export type PrimitiveFilter = {
   FD: FDsFilter;
@@ -6,3 +10,6 @@ export type PrimitiveFilter = {
   AR: ARsFilter;
   TypoFD: Pagination;
 };
+
+export type Cluster = getClustersPreview_taskInfo_data_result_TypoClusterTaskResult_TypoClusters;
+export type ClustersInfo = getClustersPreview_taskInfo_data_result_TypoClusterTaskResult;
