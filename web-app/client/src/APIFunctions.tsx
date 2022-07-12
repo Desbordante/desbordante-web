8bd56bfada87410c6ba11fa513d9ac404bfc6f93
Kirill Stupakov <kirill.stupakov.0@gmail.com>
2022-07-13 02:47:00 +0300

    Fix PR comments

--- web-app/client/src/APIFunctions.tsx
+++ web-app/client/src/APIFunctions.tsx
@@ -1,2 +1,2 @@
 export const serverURL = `${process.env.REACT_APP_SERVER_PROTOCOL}://${process.env.REACT_APP_HOST_SERVER_IP}:${process.env.REACT_APP_SERVER_PORT}`;
-export const graphQLEndpoint = `${serverURL}/graphql`;
+export const graphQLEndpoint = `${process.env.REACT_APP_SERVER_PROTOCOL}://${process.env.REACT_APP_HOST_SERVER_IP}:${process.env.REACT_APP_SERVER_PORT}/graphql`;
