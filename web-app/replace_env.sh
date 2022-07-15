dd5a3b1e90cfdeeee8f69e23eb3312364f2200e7
Ilia Shuckin <theilia509@gmail.com>
2022-07-15 07:17:02 +0300

    Fix wrong host address substitution bug in client

--- web-app/replace_env.sh
+++ web-app/replace_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find /usr/share/nginx/html/ -type f -name "*.js" -exec sed -i '' -e "s/REACT_APP_HOST_SERVER_IP/$REACT_APP_HOST_SERVER_IP/g" {} + 
+find /usr/share/nginx/html/ -type f -name "*.js" -exec sed -i '' -e "s/\"REACT_APP_HOST_SERVER_IP\"/\"$REACT_APP_HOST_SERVER_IP\"/g" {} + 
 find /usr/share/nginx/html/ -type f -name "*.js" -exec sed -i '' -e "s/REACT_APP_SERVER_PORT/$REACT_APP_SERVER_PORT/g" {} +
 find /usr/share/nginx/html/ -type f -name "*.js" -exec sed -i '' -e "s/REACT_APP_SERVER_PROTOCOL/$REACT_APP_SERVER_PROTOCOL/g" {} +
 find /usr/share/nginx/html/ -type f -name "*.js" -exec sed -i '' -e "s/REACT_APP_PASSWORD_SALT_PREFIX/$REACT_APP_PASSWORD_SALT_PREFIX/g" {} +
