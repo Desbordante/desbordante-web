b47a9ffe02e5c335b58dcebb5c82c7cc209bf8ce
Ilya <barutkinila@gmail.com>
2022-07-26 23:27:50 +0000

    wip: home screen

--- web-app/client/src/components/Button/Button.tsx
+++ web-app/client/src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 import { Button as Btn } from "react-bootstrap";
 
-import "./Button.scss";
+import styles from "./Button.module.scss";
 
 interface Props {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
@@ -20,17 +20,16 @@ const Button: React.FC<Props> = ({
   children,
   style,
   size,
-}) => (
-  <Btn
-    disabled={!enabled}
-    variant={variant}
-    className={`text-nowrap px-3 py-2 rounded-pill cursor-pointer ${className}`}
-    onClick={enabled ? onClick : () => {}}
-    style={style}
-    size={size}
-  >
-    <>{children}</>
-  </Btn>
-);
+}) => {
+  const defaultClassName = styles[variant]
+  return (
+    <span tabIndex={0}
+      className={`${defaultClassName} ${className}`}
+      onClick={enabled ? onClick : () => {}}
+    >
+      <>{children}</>
+    </span>
+  );
+}
 
 export default Button;
