be06b2b628f574a41aff9c82bc01450a928ce908
Chizhov Anton <zr9ihi@gmail.com>
2022-07-27 16:56:44 +0300

    Change app configuration, fix code

---
+++ web-app/server/src/graphql/util/index.ts
@@ -0,0 +1,28 @@
+import { ApolloError, UserInputError } from "apollo-server-core";
+
+export type Pagination = { offset: number; limit: number };
+
+const defaultMaxLimit = 500;
+
+export const applyPagination = <T>(
+    data: T[],
+    pagination: Pagination,
+    maxLimit = defaultMaxLimit
+) => {
+    const { offset, limit } = pagination;
+    if (offset < 0) {
+        throw new UserInputError("Offset cannot be less, than 0", pagination);
+    } else if (limit < 0 || limit > maxLimit) {
+        throw new UserInputError(
+            `Limit must be greater than zero, and less than maxLimit (${maxLimit})`,
+            pagination
+        );
+    }
+    return data.slice(offset, offset + limit);
+};
+
+export const returnParent = <T>(parent: T) => parent;
+
+export const resolverCannotBeCalled = () => {
+    throw new ApolloError("The resolver function cannot be called by this interface");
+};
