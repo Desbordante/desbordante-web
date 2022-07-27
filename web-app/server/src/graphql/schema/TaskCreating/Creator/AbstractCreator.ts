4c7b585ce8ec6d54beb6b2e7be447ed14a887e92
Chizhov Anton <zr9ihi@gmail.com>
2022-07-27 16:47:34 +0300

    Refactor task creating

---
+++ web-app/server/src/graphql/schema/TaskCreating/Creator/AbstractCreator.ts
@@ -0,0 +1,305 @@
+import { ApolloError, UserInputError } from "apollo-server-core";
+import {
+    DBTaskPrimitiveType,
+    SpecificPrimitiveType,
+    isMainPrimitiveType,
+    isSpecificPrimitiveType,
+} from "../../../../db/models/TaskData/configs/GeneralTaskConfig";
+import {
+    InputMaybe,
+    Scalars,
+    SpecificClusterTaskProps,
+} from "../../../../tests/__generated__/types";
+import {
+    IntersectionMainTaskProps,
+    IntersectionSpecificTaskProps,
+    MainPrimitiveType,
+} from "../../../types/types";
+import {
+    SchemaType,
+    getMainTaskSchema,
+    getSpecificClusterTaskSchema,
+    getSpecificTaskSchema,
+} from "./schema";
+import { Context } from "../../../types/context";
+import { FileInfo } from "../../../../db/models/FileData/FileInfo";
+import { TaskStatusType } from "../../../../db/models/TaskData/TaskState";
+import { allowedAlgorithms } from "../../AppConfiguration/resolvers";
+import { produce } from "../../../../producer";
+
+export type TransformedSpecificClusterTaskProps = SpecificClusterTaskProps & {
+    type: "SpecificTypoCluster";
+    parentTaskID: string;
+};
+
+export type RawPropsType =
+    | IntersectionMainTaskProps
+    | IntersectionSpecificTaskProps
+    | TransformedSpecificClusterTaskProps;
+
+export type TransformedIntersectionSpecificTaskProps = Omit<
+    IntersectionSpecificTaskProps,
+    "typoFD"
+> & { typoFD?: InputMaybe<Scalars["String"]> };
+
+export type PropsType =
+    | IntersectionMainTaskProps
+    | TransformedIntersectionSpecificTaskProps
+    | TransformedSpecificClusterTaskProps;
+
+export type InvalidUserInput<T> = {
+    property: keyof T;
+    expected: string | number;
+};
+
+export type ValidationAnswer<T> =
+    | { type: "Valid" }
+    | { type: "Error"; info: InvalidUserInput<T> };
+
+export abstract class AbstractCreator<
+    PropsType extends { [key: string]: any },
+    PrimitiveType extends DBTaskPrimitiveType
+> {
+    public constructor(
+        protected props: PropsType,
+        protected readonly type: DBTaskPrimitiveType,
+        protected context: Context,
+        protected forceCreate: boolean,
+        protected fileInfo: FileInfo,
+        protected userID?: string
+    ) {}
+
+    protected models = () => this.context.models;
+    protected logger = this.context.logger;
+
+    protected baseValidateProps = (
+        type: PrimitiveType,
+        algo: string
+    ): ValidationAnswer<PropsType> => {
+        if (!allowedAlgorithms.has(type)) {
+            throw new ApolloError(`Allowed algorithms for type '${type}' not found`);
+        }
+        if (allowedAlgorithms.get(type)!.algorithms.some(({ name }) => algo === name)) {
+            return { type: "Valid" };
+        }
+        const expectedAlgorithms = allowedAlgorithms
+            .get(type)!
+            .algorithms.map(({ name }) => name);
+        const expected = `[${expectedAlgorithms.join(", ")}]`;
+        return {
+            type: "Error",
+            info: {
+                property: "algorithmName",
+                expected,
+            },
+        };
+    };
+
+    abstract getSchema(): SchemaType<PropsType, PrimitiveType>;
+
+    public beforeValidation = async () => {
+        return;
+    };
+
+    public validateProps = async () => {
+        await this.beforeValidation();
+        const base = this.baseValidateProps(this.props.type, this.props.algorithmName);
+        if (base.type === "Error") {
+            return base;
+        }
+        for (const { info, isValid } of this.getSchema()) {
+            if (!(await isValid(this.props, this.fileInfo, this.context))) {
+                return {
+                    type: "Error" as const,
+                    info,
+                };
+            }
+        }
+        return { type: "Valid" as const };
+    };
+
+    private findSimilarTask = async () => {
+        const SpecificConfigModelName = `${this.type}TaskConfig` as const;
+        const { algorithmName, type, ...rest } = this.props;
+        const props = {
+            ...rest,
+        };
+        // @ts-ignore
+        const specificConfigs = await this.models()[SpecificConfigModelName].findAll({
+            where: { ...props },
+        });
+        if (specificConfigs.length === 0) {
+            return null;
+        }
+        for await (const config of specificConfigs) {
+            const { taskID } = config;
+            const generalConfig = await this.models().GeneralTaskConfig.findByPk(taskID);
+            if (generalConfig?.algorithmName !== algorithmName) {
+                continue;
+            }
+            const state = await this.models().TaskState.getTaskState(taskID);
+            if (state.isExecuted) {
+                return config;
+            }
+        }
+        return null;
+    };
+
+    private saveToDB = async () => {
+        const status: TaskStatusType = "ADDING_TO_DB";
+        const userID = this.userID;
+        const { fileID } = this.fileInfo;
+        const taskState = await this.context.models.TaskState.create({
+            status,
+            userID,
+        });
+        await taskState.$create("baseConfig", {
+            ...this.props,
+            fileID,
+        });
+        await taskState.$create(`${this.type}Config`, { ...this.props });
+        await taskState.$create(`${this.type}Result`, {});
+        return taskState;
+    };
+
+    private createTask = async () => {
+        if (!this.forceCreate) {
+            const similarTask = await this.findSimilarTask();
+            if (similarTask) {
+                const state = await this.context.models.TaskState.findByPk(
+                    similarTask.taskID
+                );
+                return state!;
+            }
+        }
+        const task = await this.saveToDB();
+        const { taskID } = task;
+
+        await produce({ taskID }, this.context.topicNames.tasks);
+        return task;
+    };
+
+    public processTask = async () => {
+        const validationAnswer = await this.validateProps();
+        if (validationAnswer.type === "Error") {
+            const { info } = validationAnswer;
+            const { expected, property } = info;
+            const errorMessage =
+                `Received incorrect property '${String(property)}'. ` +
+                `Expected: ${expected}'`;
+            throw new UserInputError(errorMessage, { info });
+        }
+        return await this.createTask();
+    };
+}
+
+class MainPrimitiveCreator extends AbstractCreator<
+    IntersectionMainTaskProps,
+    MainPrimitiveType
+> {
+    public getSchema = () => getMainTaskSchema(this.props.type);
+}
+
+abstract class SpecificPrimitiveCreator extends AbstractCreator<
+    TransformedIntersectionSpecificTaskProps,
+    SpecificPrimitiveType
+> {
+    getSchema = () => getSpecificTaskSchema(this.props.type);
+}
+
+class TypoClusterCreator extends SpecificPrimitiveCreator {
+    public beforeValidation = async () => {
+        const { radius, ratio } = this.props;
+        if (radius != undefined && ratio != undefined) {
+            return;
+        }
+        if (!this.props.parentTaskID) {
+            return;
+        }
+        const parentSpecificConfig = await this.models().TypoFDTaskConfig.findByPk(
+            this.props.parentTaskID
+        );
+        if (!parentSpecificConfig) {
+            return;
+        }
+        const { defaultRadius, defaultRatio } = parentSpecificConfig;
+        this.props.radius = radius || defaultRadius;
+        this.props.ratio = ratio || defaultRatio;
+    };
+}
+
+class SpecificClusterCreator extends AbstractCreator<
+    TransformedSpecificClusterTaskProps,
+    "SpecificTypoCluster"
+> {
+    getSchema(): SchemaType<TransformedSpecificClusterTaskProps, "SpecificTypoCluster"> {
+        return getSpecificClusterTaskSchema();
+    }
+}
+
+const isIntersectionMainTaskProps = (
+    props: PropsType
+): props is IntersectionMainTaskProps => {
+    return isMainPrimitiveType(props.type);
+};
+
+const isIntersectionSpecificTaskProps = (
+    props: PropsType
+): props is TransformedIntersectionSpecificTaskProps => {
+    return isSpecificPrimitiveType(props.type);
+};
+
+const isSpecificClusterTaskProps = (
+    props: PropsType
+): props is TransformedSpecificClusterTaskProps => {
+    return props.type === "SpecificTypoCluster";
+};
+
+export class TaskCreatorFactory {
+    public static getSpecificPrimitiveConstructor = (props: PropsType) => {
+        if ("type" in props) {
+            if (isSpecificPrimitiveType(props.type)) {
+                switch (props.type) {
+                    case "TypoCluster":
+                        return TypoClusterCreator;
+                }
+            }
+        }
+        throw new Error("Not implemented yet");
+    };
+
+    public static transformRawProps = (props: RawPropsType): PropsType => {
+        let transformedProps: PropsType | undefined;
+        if (props.type === "TypoCluster") {
+            const { typoFD, ...rest } = props;
+            transformedProps = { ...rest, typoFD: typoFD?.join(",") };
+        } else {
+            transformedProps = { ...props };
+        }
+        return transformedProps;
+    };
+
+    public static build = async (
+        type: DBTaskPrimitiveType,
+        context: Context,
+        rawProps: RawPropsType,
+        fileInfo: FileInfo,
+        forceCreate = false
+    ) => {
+        const props = this.transformRawProps(rawProps);
+        const otherProps = [type, context, forceCreate, fileInfo] as const;
+        const creatorInstance = await (async () => {
+            if (isIntersectionMainTaskProps(props)) {
+                return new MainPrimitiveCreator(props, ...otherProps);
+            } else if (isIntersectionSpecificTaskProps(props)) {
+                const TaskCreatorConstructor =
+                    this.getSpecificPrimitiveConstructor(props);
+                return new TaskCreatorConstructor(props, ...otherProps);
+            } else if (isSpecificClusterTaskProps(props)) {
+                return new SpecificClusterCreator(props, ...otherProps);
+            }
+            throw new ApolloError("Unreachable code");
+        })();
+        return await creatorInstance.processTask();
+    };
+}
--- web-app/server/src/graphql/schema/TaskCreating/csvValidator.ts
+++ web-app/server/src/graphql/schema/TaskCreating/csvValidator.ts
@@ -1,16 +1,19 @@
 import { UserInputError } from "apollo-server-core";
+import config from "../../../config";
 import fs from "fs";
 import readline from "readline";
 
-import { fileConfig } from "../AppConfiguration/resolvers";
-import { InputFileFormat } from "../../types/types";
-
-function csvLinetoArray (line: string, sep: string) {
-    if (sep=="|") {
+function csvLinetoArray(line: string, sep: string) {
+    if (sep == "|") {
         sep = "\\|";
     }
-    const re_valid = new RegExp(`^\\s*(?:'[^'\\\\]*(?:\\\\[\\S\\s][^'\\\\]*)*'|"[^"\\\\]*(?:\\\\[\\S\\s][^"\\\\]*)*"|[^${sep}'"\\s\\\\]*(?:\\s+[^${sep}'"\\s\\\\]+)*)\\s*(?:${sep}\\s*(?:'[^'\\\\]*(?:\\\\[\\S\\s][^'\\\\]*)*'|"[^"\\\\]*(?:\\\\[\\S\\s][^"\\\\]*)*"|[^${sep}'"\\s\\\\]*(?:\\s+[^${sep}'"\\s\\\\]+)*)\\s*)*$`);
-    const re_value = new RegExp(`(?!\\s*$)\\s*(?:'([^'\\\\]*(?:\\\\[\\S\\s][^'\\\\]*)*)'|"([^"\\\\]*(?:\\\\[\\S\\s][^"\\\\]*)*)"|([^${sep}'"\\s\\\\]*(?:\\s+[^${sep}'"\\s\\\\]+)*))\\s*(?:${sep}|$)`, "g");
+    const re_valid = new RegExp(
+        `^\\s*(?:'[^'\\\\]*(?:\\\\[\\S\\s][^'\\\\]*)*'|"[^"\\\\]*(?:\\\\[\\S\\s][^"\\\\]*)*"|[^${sep}'"\\s\\\\]*(?:\\s+[^${sep}'"\\s\\\\]+)*)\\s*(?:${sep}\\s*(?:'[^'\\\\]*(?:\\\\[\\S\\s][^'\\\\]*)*'|"[^"\\\\]*(?:\\\\[\\S\\s][^"\\\\]*)*"|[^${sep}'"\\s\\\\]*(?:\\s+[^${sep}'"\\s\\\\]+)*)\\s*)*$`
+    );
+    const re_value = new RegExp(
+        `(?!\\s*$)\\s*(?:'([^'\\\\]*(?:\\\\[\\S\\s][^'\\\\]*)*)'|"([^"\\\\]*(?:\\\\[\\S\\s][^"\\\\]*)*)"|([^${sep}'"\\s\\\\]*(?:\\s+[^${sep}'"\\s\\\\]+)*))\\s*(?:${sep}|$)`,
+        "g"
+    );
 
     if (!re_valid.test(line)) {
         throw new UserInputError("Invalid csv string was provided", { line });
@@ -20,7 +23,7 @@ function csvLinetoArray (line: string, sep: string) {
         if (m1 !== undefined) {
             a.push(m1.replace(/\\'/g, "'"));
         } else if (m2 !== undefined) {
-            a.push(m2.replace(/\\"/g, "\""));
+            a.push(m2.replace(/\\"/g, '"'));
         } else if (m3 !== undefined) {
             a.push(m3);
         }
@@ -32,13 +35,16 @@ function csvLinetoArray (line: string, sep: string) {
     return a;
 }
 
-function getNumberOfColumns (line: string, sep: string) {
+function getNumberOfColumns(line: string, sep: string) {
     return csvLinetoArray(line, sep).length;
 }
 
-async function tryFindCorrectSeparator (path: fs.PathLike) {
+async function tryFindCorrectSeparator(path: fs.PathLike) {
     const fileStream = fs.createReadStream(path);
-    const rl = readline.createInterface({ input: fileStream, crlfDelay: Infinity });
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
 
     const lines: string[] = [];
     for await (const line of rl) {
@@ -55,14 +61,17 @@ async function tryFindCorrectSeparator (path: fs.PathLike) {
         let countOfColumns = -1;
         for (const line of lines) {
             const curColsNumber = getNumberOfColumns(line, sep);
-            if (curColsNumber === 1
-                || ~countOfColumns && countOfColumns !== curColsNumber) {
+            if (
+                curColsNumber === 1 ||
+                (~countOfColumns && countOfColumns !== curColsNumber)
+            ) {
                 return false;
             }
             countOfColumns = curColsNumber;
         }
         return true;
     };
+    const { fileConfig } = config.appConfig;
     for (const sep of fileConfig.allowedDelimiters) {
         if (checkSeparator(sep, lines)) {
             return sep;
@@ -72,21 +81,27 @@ async function tryFindCorrectSeparator (path: fs.PathLike) {
 }
 
 export type TabularFileFormatProps = {
-    inputFormat: InputFileFormat.Tabular
-    hasTid: boolean
-}
+    inputFormat: "TABULAR";
+    hasTid: boolean;
+};
 
 export type SingularFileFormatProps = {
-    inputFormat: InputFileFormat.Singular
-    tidColumnIndex: number
-    itemColumnIndex: number
-}
+    inputFormat: "SINGULAR";
+    tidColumnIndex: number;
+    itemColumnIndex: number;
+};
 
-export const findRowsAndColumnsNumber = async (path: fs.PathLike, sep: string,
-    fileFormat: TabularFileFormatProps | SingularFileFormatProps | undefined = undefined) => {
+export const findRowsAndColumnsNumber = async (
+    path: fs.PathLike,
+    sep: string,
+    fileFormat: TabularFileFormatProps | SingularFileFormatProps | undefined = undefined
+) => {
     const fileStream = fs.createReadStream(path);
 
-    const rl = readline.createInterface({ input: fileStream, crlfDelay: Infinity });
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
     let rowsCount = 0;
     let countOfColumns: null | number = -1;
     for await (const line of rl) {
@@ -95,7 +110,9 @@ export const findRowsAndColumnsNumber = async (path: fs.PathLike, sep: string,
         const curColsNumber = cols.length;
         if (!fileFormat && curColsNumber === 1) {
             const sep = await tryFindCorrectSeparator(path);
-            let errorMessage = "Either the table has 1 column or an invalid delimiter is specified.";
+            let errorMessage =
+                "Either the table has 1 column or" +
+                " an invalid delimiter is specified.";
             if (sep) {
                 errorMessage += ` Maybe you want to provide delimiter '${sep}'`;
             }
@@ -103,15 +120,37 @@ export const findRowsAndColumnsNumber = async (path: fs.PathLike, sep: string,
         }
         if (!fileFormat && ~countOfColumns && countOfColumns !== curColsNumber) {
             throw new UserInputError(
-                `Row ${rowsCount} has ${curColsNumber} columns, `
-                + `but row ${rowsCount - 1} has ${countOfColumns} columns.`);
-        } else if (fileFormat && fileFormat.inputFormat === "SINGULAR" && ~countOfColumns && countOfColumns < Math.max(fileFormat.tidColumnIndex, fileFormat.itemColumnIndex)) {
-            throw new UserInputError(`Row ${rowsCount} has ${curColsNumber} cols, but it must be less, than Tid column index and Item column index`);
-        } else if (fileFormat && fileFormat.inputFormat === "TABULAR" && ~countOfColumns && fileFormat.hasTid && (countOfColumns < 2 || Number.isNaN(Number.parseInt(cols[0])))) {
+                `Row ${rowsCount} has ${curColsNumber} columns, ` +
+                    `but row ${rowsCount - 1} has ${countOfColumns} columns.`
+            );
+        } else if (
+            fileFormat &&
+            fileFormat.inputFormat === "SINGULAR" &&
+            ~countOfColumns &&
+            countOfColumns <
+                Math.max(fileFormat.tidColumnIndex, fileFormat.itemColumnIndex)
+        ) {
+            throw new UserInputError(
+                `Row ${rowsCount} has ${curColsNumber} cols, ` +
+                    "but it must be less, than Tid column index and Item column index"
+            );
+        } else if (
+            fileFormat &&
+            fileFormat.inputFormat === "TABULAR" &&
+            ~countOfColumns &&
+            fileFormat.hasTid &&
+            (countOfColumns < 2 || Number.isNaN(Number.parseInt(cols[0], 10)))
+        ) {
             if (countOfColumns < 2) {
-                throw new UserInputError(`Row ${rowsCount} has ${curColsNumber} cols, but it must have more, than 1 columns (tid and items columns)`);
+                throw new UserInputError(
+                    `Row ${rowsCount} has ${curColsNumber} cols, ` +
+                        "but it must have more, than 1 columns (tid and items columns)"
+                );
             } else {
-                throw new UserInputError(`Expected, that ${cols[0]} is integer (Transaction column id is ${1})`);
+                throw new UserInputError(
+                    `Expected, that ${cols[0]} is integer ` +
+                        ` (Transaction column id is ${1})`
+                );
             }
         } else {
             countOfColumns = curColsNumber;
@@ -121,23 +160,32 @@ export const findRowsAndColumnsNumber = async (path: fs.PathLike, sep: string,
     return { rowsCount, countOfColumns };
 };
 
-export const createTabularFileFromSingular = async (path: fs.PathLike, sep: string,
-                                                    tidColumnIndex: number, itemColumnIndex: number) => {
+export const createTabularFileFromSingular = async (
+    path: fs.PathLike,
+    sep: string,
+    tidColumnIndex: number,
+    itemColumnIndex: number
+) => {
     const fileStream = fs.createReadStream(path);
     const newPath = path.toString().replace(".csv", "_SINGULAR.csv");
     const writeFileStream = fs.createWriteStream(newPath);
 
-    const rl = readline.createInterface({ input: fileStream, crlfDelay: Infinity });
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
 
     const usedTids = new Set<number>();
     const currentItemset = new Array<string>();
     let currentTid: number | undefined;
     for await (const line of rl) {
         const columns = csvLinetoArray(line, sep);
-        const tid = parseInt(columns[tidColumnIndex - 1]);
+        const tid = parseInt(columns[tidColumnIndex - 1], 10);
         if (Number.isNaN(tid)) {
-            throw new UserInputError(`Incorrect line ${line}.`
-                + ` Expected, that ${columns[tidColumnIndex - 1]} is integer value`);
+            throw new UserInputError(
+                `Incorrect line ${line}.` +
+                    ` Expected, that ${columns[tidColumnIndex - 1]} is integer value`
+            );
         }
         const item = columns[itemColumnIndex - 1];
         if (currentTid === undefined) {
@@ -154,14 +202,20 @@ export const createTabularFileFromSingular = async (path: fs.PathLike, sep: stri
             currentTid = tid;
             currentItemset.push(item);
         } else {
-            throw new UserInputError(`It was expected that all records for a transaction with tid '${currentTid}' would go sequentially`);
+            throw new UserInputError(
+                "It was expected that all records for a " +
+                    `transaction with tid '${currentTid}' would go sequentially`
+            );
         }
     }
     if (currentTid !== undefined && !usedTids.has(currentTid)) {
         usedTids.add(currentTid);
         writeFileStream.write(`${currentTid},${currentItemset.join(",")}\n`);
     } else {
-        throw new UserInputError(`It was expected that all records for a transaction with tid '${currentTid}' would go sequentially`);
+        throw new UserInputError(
+            "It was expected that all records for a transaction" +
+                `with tid '${currentTid}' would go sequentially`
+        );
     }
     return newPath;
 };
