7c49f14e23638c856b56c7a85b6db240f929ec17
Ilya <barutkinila@gmail.com>
2022-07-29 22:24:33 +0000

    fix merge conflicts

---
+++ web-app/client/APIFunctions.tsx
@@ -0,0 +1,2 @@
+export const serverURL = `${process.env.REACT_APP_SERVER_PROTOCOL}://${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`;
+export const graphQLEndpoint = `${serverURL}/graphql`;
