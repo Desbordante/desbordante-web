5e6c8d24c1c6a07a89c5c3c7663078432de0c89c
Ilya <barutkinila@gmail.com>
2022-07-30 20:13:57 +0000

    delete bootstrap.css import, make it injected to the head when opening the sign up form

--- web-app/client/components/Layout/Layout.tsx
+++ web-app/client/components/Layout/Layout.tsx
@@ -13,7 +13,6 @@ const Layout: FCWithChildren = ({ children }) => {
       <Head>
         <title>Desbordante | Open-source Data Profiling Tool</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
       </Head>
       <Header />
       {isSignUpShown && <SignUpForm />}
--- web-app/client/components/SignUpForm/index.tsx
+++ web-app/client/components/SignUpForm/index.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { Container } from "react-bootstrap";
+import Head from 'next/head'
 import PopupWindowContainer from "@components/PopupWindowContainer/PopupWindowContainer";
 import { AuthContext } from "@components/AuthContext";
 import StageOne from "./SignUpForm";
@@ -10,7 +11,10 @@ const SignUpForm = () => {
   const { setIsSignUpShown, user } = useContext(AuthContext)!;
   const [stage, setStage] = useState(user?.id && !user?.isVerified ? 2 : 1);
   const goToNextStage = () => setStage((prevStage) => prevStage + 1);
-  return (
+  return (<>
+    <Head>          
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" />
+    </Head>
     <PopupWindowContainer onOutsideClick={() => setIsSignUpShown(false)}>
       <Container className="form-container bg-light m-4 m-sm-5 p-4 p-sm-5 rounded-3 w-auto shadow-lg">
         {stage === 1 && <StageOne onSuccess={goToNextStage} />}
@@ -19,7 +23,7 @@ const SignUpForm = () => {
           <WelcomeMessage onClose={() => setIsSignUpShown(false)} />
         )}
       </Container>
-    </PopupWindowContainer>
+    </PopupWindowContainer></>
   );
 };
 
