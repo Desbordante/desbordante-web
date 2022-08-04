8532e158dc00a58f8bcb6e97058efaf9fa103bff
Ilya <barutkinila@gmail.com>
2022-08-04 14:49:31 +0000

    refactor datasets card, set up new background

---
+++ web-app/client/components/DatasetCard/DatasetCard.tsx
@@ -0,0 +1,47 @@
+import { limitString } from "@utils/strings"
+import { BaseHTMLAttributes, FC } from "react"
+import { AllowedDataset } from "types/algorithms"
+import threeDots from '@assets/icons/three-dots.svg';
+import classNames from "classnames";
+import styles from './DatasetCard.module.scss';
+
+interface DatasetCardProps extends BaseCardProps {
+    file: AllowedDataset,
+}
+
+interface FileCardProps extends BaseCardProps {
+    file: File,
+}
+interface BaseCardProps extends BaseHTMLAttributes<HTMLDivElement> {
+    isSelected?: boolean
+}
+
+export const BuiltInDatasetCard: FC<DatasetCardProps> = ({file, ...rest}) => {
+    const fileDescription = `421 KB: ${file.rowsCount} rows, ${file.countOfColumns} columns`
+    return (
+    <BaseCard {...rest}>
+        <div className={styles.card_title}><p>{limitString(file.fileName, 14)}</p><img src={threeDots.src} width={20} /></div>
+        <div className={styles.card_description}>
+          <span className={fileDescription.length > 27 ? styles.large_content : undefined}>{fileDescription}</span>
+          <span>Uploaded 15 minutes ago</span>
+        </div>
+    </BaseCard>
+    )
+}
+
+export const FileCard: FC<FileCardProps> = ({file, ...rest}) => {
+    return (
+    <BaseCard {...rest}>
+        <div className={styles.card_title}><p>{limitString(file.name, 14)}</p><img src={threeDots.src} width={20} /></div>
+        <div className={styles.card_description}>
+          <span>{file.size < 1024 ? `${file.size} B` : `${(file.size / 1024).toFixed(2)} KB`}</span>
+          <span>File is not uploaded yet</span>
+        </div>
+    </BaseCard>
+    )
+}
+  
+
+export const BaseCard: FC<BaseCardProps> = ({children, isSelected=false, ...rest}) => {
+    return (<div className={classNames(styles.card, isSelected ? styles.selected : null)} {...rest}>{children}</div>)
+}
--- web-app/client/pages/choose-file.tsx
+++ web-app/client/pages/choose-file.tsx
@@ -1,29 +1,36 @@
 import type { NextPage } from 'next';
-import Button from '@components/Button';
-import ExternalLink from '@components/ExternalLink';
-import styles from '@styles/ChooseFile.module.scss';
-import plexus from '@public/plexus.jpg';
-import threeDots from '@assets/icons/three-dots.svg';
-import arrowDown from '@assets/icons/arrow-down.svg';
-import uploadIcon from '@assets/icons/upload.svg';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { AuthContext } from '@components/AuthContext';
 import { useQuery } from '@apollo/client';
 import { getAlgorithmsConfig } from '@graphql/operations/queries/__generated__/getAlgorithmsConfig';
 import { GET_ALGORITHMS_CONFIG } from '@graphql/operations/queries/getAlgorithmsConfig';
 import { ErrorContext } from '@components/ErrorContext';
-import { limitString } from '@utils/strings';
+import { AllowedDataset } from 'types/algorithms';
+import _ from 'lodash'
+import { BaseCard, BuiltInDatasetCard, FileCard } from '@components/DatasetCard/DatasetCard';
+import styles from '@styles/ChooseFile.module.scss';
+import bg from '@public/bg.svg';
+import arrowDown from '@assets/icons/arrow-down.svg';
+import uploadIcon from '@assets/icons/upload.svg';
+import { MainPrimitiveType } from 'types/globalTypes';
 
-const ChooseFile: NextPage = () => {
+
+interface ChooseFileProps {
+  primivite?: MainPrimitiveType
+}
+
+const ChooseFile: NextPage<ChooseFileProps> = ({primivite = MainPrimitiveType.FD}) => {
   const {user} = useContext(AuthContext)!
   const {showError} = useContext(ErrorContext)!
-
   const { loading, data, error } = useQuery<getAlgorithmsConfig>(
     GET_ALGORITHMS_CONFIG
   );
+  const allowedDatasets = (data?.algorithmsConfig?.allowedDatasets || []).filter(e => e.supportedPrimitives.includes(primivite))
 
-  const builtInDatasets = data?.algorithmsConfig?.allowedDatasets
-
+  const [builtInDatasets, userDatasets] = _.partition(allowedDatasets, e => e.isBuiltIn)
+  const [uploadingFile, setUploadingFile] = useState<File>()
+  const [selection, setSelection] = useState<AllowedDataset|File>()
+  const inputFile = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (error) {
@@ -36,45 +43,48 @@ const ChooseFile: NextPage = () => {
 
   const userFiles = (
     <>
-        <h5>My Files <img src={arrowDown.src} width={20} /></h5>
-        {user?.permissions.canUploadFiles && (
-          <div className={styles.files}>
-            <div className={styles.card}>
-              <div className={styles.uploader_title}><img src={uploadIcon.src} width={20} /><p>Upload a File</p></div>
-            </div>
-            <div className={`${styles.card} ${styles.selected}`}>
-              <div className={styles.card_title}><p>my-data-file.csv</p><img src={threeDots.src} width={20} /></div>
-              <div className={styles.card_description}>
-                <span>421 KB: 152 rows, 13 columns</span>
-                <span>Uploaded 15 minutes ago</span>
-              </div>
-            </div>
-          </div>
-        ) || <p>You must be authorized to upload files</p>}
+      <h5>My Files <img src={arrowDown.src} width={20} /></h5>
+      {user?.permissions.canUploadFiles && (
+        <div className={styles.files}>
+          <BaseCard>
+            <div className={styles.uploader_title} onClick={() => inputFile?.current?.click()}><img src={uploadIcon.src} width={20} /><p>Upload a File</p></div>
+            <input
+              type="file"
+              id="file"
+              ref={inputFile}
+              style={{display: "none"}}
+              onChange={
+                e => {
+                  if (e.target.files) {
+                    setUploadingFile(e.target.files[0])
+                    setSelection(e.target.files[0])
+                  }
+                }
+              }
+              multiple={false}
+              accept=".csv, .CSV"
+            />
+          </BaseCard>    
+          {uploadingFile && (
+            <FileCard isSelected={selection === uploadingFile} onClick={() => setSelection(uploadingFile)} file={uploadingFile} />
+          )}
 
+          {user?.permissions.canUploadFiles && userDatasets && userDatasets.map(file => <BuiltInDatasetCard isSelected={selection === file} onClick={() => setSelection(file)} file={file} />)} 
+        </div>) || <p>You must be authorized to upload files</p>}
     </>)
   const datasets = (
     <>
       <h5>Built-in Datasets <img src={arrowDown.src} width={20} /></h5>
       <div className={styles.files}>
-        {user?.permissions.canUseBuiltinDatasets && builtInDatasets && builtInDatasets.map(file => (
-            <div className={styles.card}>
-              <div className={styles.card_title}><p>{limitString(file.fileName, 14)}</p><img src={threeDots.src} width={20} /></div>
-              <div className={styles.card_description}>
-                <span>421 KB: {file.rowsCount} rows, {file.countOfColumns} columns</span>
-                <span>Uploaded 15 minutes ago</span>
-              </div>
-            </div>
-        ))}
+        {user?.permissions.canUseBuiltinDatasets && builtInDatasets && builtInDatasets.map(file => <BuiltInDatasetCard isSelected={selection === file} onClick={() => setSelection(file)} file={file} />)}
       </div>
-
     </>)
   return (
     <div className={styles.home}>
 
       <div className={styles.background}>
         <img
-          src={plexus.src}
+          src={bg.src}
           className={styles.background_image}
           alt="background"
         />
