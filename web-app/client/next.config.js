d70b282353733e408e2223a93d8b73d0d36f8962
Kirill Stupakov <kirill.stupakov.0@gmail.com>
2022-08-05 21:58:59 +0300

    Add stylelint

--- web-app/client/next.config.js
+++ web-app/client/next.config.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const StylelintPlugin = require("stylelint-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -8,4 +9,8 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  webpack: (config) => {
+    config.plugins.push(new StylelintPlugin());
+    return config;
+  }
 };
