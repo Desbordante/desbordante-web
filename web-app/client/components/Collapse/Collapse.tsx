63e7bbd1ff855c78e8952956b816283acf1d358f
Ilya <barutkinila@gmail.com>
2022-08-05 00:12:20 +0000

    Wizard layout component

--- web-app/client/components/Collapse/Collapse.tsx
+++ web-app/client/components/Collapse/Collapse.tsx
@@ -1,14 +1,15 @@
-import { FC, PropsWithChildren, ReactElement, useState } from "react"
+import { BaseHTMLAttributes, FC, PropsWithChildren, ReactElement, useState } from "react"
 import arrowDown from '@assets/icons/arrow-down.svg';
 
 interface Props extends PropsWithChildren {
-    title: ReactElement
+    title: string,
+    titleProps: BaseHTMLAttributes<HTMLHeadingElement>
 }
 
-export const Collapse: FC<Props> = ({title, children}) => {
+export const Collapse: FC<Props> = ({title, titleProps, children}) => {
     const [isShown, setIsShown] = useState(true)!
     return <>
-        <div onClick={() => setIsShown(!isShown)}><h5>{title} <img style={{transform: isShown ? undefined : "rotate(180deg)"}} src={arrowDown.src} width={20} /></h5></div>
+        <div onClick={() => setIsShown(!isShown)}><h5 {...titleProps}>{title} <img style={{transform: isShown ? undefined : "rotate(180deg)"}} src={arrowDown.src} width={20} /></h5></div>
         <div style={{display: isShown ? "block" : "none"}}>{children}</div>
     </>
 }
