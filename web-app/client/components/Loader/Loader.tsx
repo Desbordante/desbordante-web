e69b589014650a43802d71edaabe1adfccea664d
Ilya <barutkinila@gmail.com>
2022-09-06 19:59:47 +0000

    Refactor Loader page

--- web-app/client/components/Loader/Loader.tsx
+++ web-app/client/components/Loader/Loader.tsx
@@ -7,7 +7,7 @@ import usersQueue from '@assets/icons/users-queue.svg';
 import checkFill from '@assets/icons/check-fill.svg';
 import resourcesLimit from '@assets/icons/resources-limit.svg';
 import Image from 'next/image';
-import { useQuery } from '@apollo/client';
+import { ApolloError, useQuery } from '@apollo/client';
 import { GET_TASK_INFO } from '@graphql/operations/queries/getTaskInfo';
 import {
   getTaskInfo,
@@ -25,71 +25,94 @@ type Status = {
   description: string;
   isAnimated?: boolean;
 };
+const getStatusDetails = (
+  error: ApolloError | undefined,
+  taskInfo: getTaskInfo | undefined
+) => {
+  const state = taskInfo?.taskInfo.state;
 
+  if (error) {
+    return {
+      icon: serverError,
+      label: 'Internal Server Error',
+      className: 'error',
+      description: 'Something went wrong with our server',
+    };
+  }
+  if (!state) {
+    return {
+      icon: usersQueue,
+      label: 'Queued',
+      className: 'queued',
+      description: 'Task is waiting to be executed',
+    };
+  }
+  if (state.__typename === 'TaskState') {
+    if (state.processStatus === 'COMPLETED') {
+      return {
+        icon: checkFill,
+        label: 'Completed',
+        className: 'completed',
+        description: 'You will see the results in a moment',
+      };
+    }
+
+    if (state.processStatus === 'ADDING_TO_DB') {
+      return {
+        icon: '/animated_logo.webm',
+        isAnimated: true,
+        label: 'In Progress',
+        className: 'progress',
+        description: `Adding task to the database`,
+      };
+    }
+    if (state.processStatus === 'IN_PROCESS') {
+      return {
+        icon: '/animated_logo.webm',
+        isAnimated: true,
+        label: 'In Progress',
+        className: 'progress',
+        description: `Step ${state.currentPhase} of ${state.maxPhase}: ${state.phaseName}`,
+      };
+    }
+  }
+  if (state.__typename === 'InternalServerTaskError') {
+    return {
+      icon: serverError,
+      label: 'Internal Server Error',
+      className: 'error',
+      description: 'Something went wrong with our server',
+    };
+  }
+  if (state.__typename === 'ResourceLimitTaskError') {
+    return {
+      label: 'Resource Limit Reached',
+      icon: resourcesLimit,
+      className: 'resources-limit',
+      description: 'Our server ran out of resources while executing your task',
+    };
+  }
+
+  return {
+    icon: '/animated_logo.webm',
+    isAnimated: true,
+    label: 'In Progress',
+    className: 'progress',
+    description: '',
+  };
+};
 const Loader: FC<Props> = ({ taskID }) => {
-  const { loading, data, error, startPolling } = useQuery<
+  const { data, error, startPolling } = useQuery<
     getTaskInfo,
     getTaskInfoVariables
   >(GET_TASK_INFO, { variables: { taskID } });
 
   const router = useRouter();
-  const [status, setStatus] = useState<Status>({
-    icon: usersQueue,
-    label: 'Queued',
-    className: 'queued',
-    description: 'Task is waiting to be executed',
-  });
-  const state = data?.taskInfo.state;
+  const status = getStatusDetails(error, data);
 
   useEffect(() => {
-    if (error) {
-      setStatus({
-        icon: serverError,
-        label: 'Internal Server Error',
-        className: 'error',
-        description: 'Something went wrong with our server',
-      });
-    }
-    if (!state) return;
-    if (state.__typename === 'TaskState') {
-      if (state.processStatus === 'COMPLETED') {
-        setStatus({
-          icon: checkFill,
-          label: 'Completed',
-          className: 'completed',
-          description: 'You will see the results in a moment',
-        });
-      }
-      if (state.processStatus === 'IN_PROCESS') {
-        setStatus({
-          icon: animatedLogo,
-          isAnimated: true,
-          label: 'In Progress',
-          className: 'progress',
-          description: `Step ${state.currentPhase} of ${state.maxPhase}: ${state.phaseName}`,
-        });
-      }
-    }
-    if (state.__typename === 'InternalServerTaskError') {
-      setStatus({
-        icon: serverError,
-        label: 'Internal Server Error',
-        className: 'error',
-        description: 'Something went wrong with our server',
-      });
-    }
-    if (state.__typename === 'ResourceLimitTaskError') {
-      setStatus({
-        label: 'Resource Limit Reached',
-        icon: resourcesLimit,
-        className: 'resources-limit',
-        description:
-          'Our server ran out of resources while executing your task',
-      });
-    }
-  }, [data?.taskInfo.state]);
+    const state = data?.taskInfo.state;
 
-  useEffect(() => {
     if (
       state?.__typename === 'TaskState' &&
       state.processStatus === 'COMPLETED'
@@ -113,13 +136,11 @@ const Loader: FC<Props> = ({ taskID }) => {
 
   const icon = status.isAnimated ? (
     <video autoPlay muted loop width={70} height={76}>
-      <source src="/animated_logo.webm" type="video/mp4" />
+      <source src={status.icon} type="video/mp4" />
     </video>
   ) : (
     <Image src={status.icon} alt="status" width={70} height={76} />
   );
-  if (!status) return <></>;
-
   return (
     <div className={styles.container}>
       {icon}
