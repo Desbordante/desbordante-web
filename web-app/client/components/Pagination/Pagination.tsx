6923d1a71ee680d969741b81e6a3d4160ce512b0
Ilya <barutkinila@gmail.com>
2022-09-28 22:46:39 +0000

    Add pagination for dependencies

--- web-app/client/components/Pagination/Pagination.tsx
+++ web-app/client/components/Pagination/Pagination.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, PropsWithChildren } from "react";
 import _ from "lodash";
 import styles from "./Pagination.module.scss";
 import arrowRight from "@assets/icons/arrow-right.svg";
@@ -8,29 +8,50 @@ import classNames from "classnames";
 type Props = {
   count: number;
   current: number;
+  onChange: (page: number) => void;
 };
 
-const Pagination: FC<Props> = ({ current, count }) => {
+type PageProps = {
+  page: number;
+} & PropsWithChildren;
+
+const createPage: (onChange: (page: number) => void) => FC<PageProps> =
+  (onChange) =>
+  ({ children, page }) =>
+    (
+      <div onClick={() => onChange(page)} className={styles.page}>
+        {children || page}
+      </div>
+    );
+
+const Pagination: FC<Props> = ({ current, count, onChange }) => {
+  const Page = createPage(onChange);
   return (
     <div className={styles.container}>
-      <div className={classNames(styles.button, styles.left)}>
+      <div
+        className={classNames(styles.button, styles.left)}
+        onClick={() => onChange(Math.max(1, current - 1))}
+      >
         <Image src={arrowRight} width={16} height={16} />
       </div>
-      {current !== 1 && <div className={styles.page}>{1}</div>}
+      {current !== 1 && <Page page={1} />}
       {current > 4 && <div className={styles.page}>...</div>}
 
-      {current - 2 > 1 && <div className={styles.page}>{current - 2}</div>}
-      {current - 1 > 1 && <div className={styles.page}>{current - 1}</div>}
+      {current - 2 > 1 && <Page page={current - 2} />}
+      {current - 1 > 1 && <Page page={current - 1} />}
 
       <div className={classNames(styles.page, styles.active)}>{current}</div>
 
-      {current + 1 < count && <div className={styles.page}>{current + 1}</div>}
-      {current + 2 < count && <div className={styles.page}>{current + 2}</div>}
+      {current + 1 < count && <Page page={current + 1} />}
+      {current + 2 < count && <Page page={current + 2} />}
 
       {current < count - 3 && <div className={styles.page}>...</div>}
 
-      {current !== count && <div className={styles.page}>{count}</div>}
-      <div className={styles.button}>
+      {current !== count && <Page page={count} />}
+      <div
+        className={styles.button}
+        onClick={() => onChange(Math.min(count, current + 1))}
+      >
         <Image src={arrowRight} width={16} height={16} />
       </div>
     </div>
