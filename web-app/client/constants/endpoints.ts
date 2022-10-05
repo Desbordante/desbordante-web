4184ffe6e6ae13af51d0abbd856b8454b75a027e
Ilya <barutkinila@gmail.com>
2022-10-05 20:15:19 +0000

    Add SSR and filters window

--- web-app/client/constants/endpoints.ts
+++ web-app/client/constants/endpoints.ts
@@ -1,4 +1,6 @@
-import { serverIP, serverPort, serverProtocol } from '@utils/env';
+import { serverIP, serverPort, serverProtocol } from "@utils/env";
 
 export const serverURL = `${serverProtocol}://${serverIP}:${serverPort}`;
+export const serverLocalURL = `${serverProtocol}://localhost:${serverPort}`;
 export const graphQLEndpoint = `${serverURL}/graphql`;
+export const graphQLLocalEndpoint = `${serverLocalURL}/graphql`;
