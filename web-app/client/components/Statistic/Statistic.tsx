95d03e63da14a07ed5a69c9efda24ede7810724e
VladYoSlav <vaifaer@gmail.com>
2022-11-04 19:06:40 +0300

    Add adaptivity for `ColumnCard`

---
+++ web-app/client/components/Statistic/Statistic.tsx
@@ -0,0 +1,26 @@
+import { FC, HTMLProps } from "react";
+import styles from "./Statistic.module.scss";
+import classNames from "classnames";
+
+export type StatisticSize = "compact" | "big";
+
+type StatisticProps = {
+  size?: StatisticSize;
+  header?: string;
+} & Omit<HTMLProps<HTMLDivElement>, "size">;
+
+export const Statistic: FC<StatisticProps> = ({
+  size,
+  className,
+  children,
+  header,
+  ...props
+}: StatisticProps) => (
+  <article
+    className={classNames(className, styles.wrapper, size && styles[size])}
+    {...props}
+  >
+    <span className={styles.header}>{header}</span>
+    <span className={styles.inner}>{children}</span>
+  </article>
+);
