043d6ef2e2f4bbe1c07fb4ae9e3ab51b0112da50
VladYoSlav <vaifaer@gmail.com>
2022-11-04 21:22:24 +0300

    Add table mode for `ColumnCard`

--- web-app/client/components/ColumnCard/ColumnCard.tsx
+++ web-app/client/components/ColumnCard/ColumnCard.tsx
@@ -6,38 +6,66 @@ import { getFileStats_fileStats } from "@graphql/operations/queries/__generated_
 import Button from "@components/Button";
 import chevronDown from "@assets/icons/chevron-down.svg";
 import chevronUp from "@assets/icons/chevron-up.svg";
+import grid from "@assets/icons/grid.svg";
+import list from "@assets/icons/list.svg";
 import { Collapse } from "react-collapse";
+import classNames from "classnames";
 
 type ColumnCardProps = {
   column: getFileStats_fileStats;
 };
 
+enum StatsMode {
+  Blocks,
+  Table,
+}
+
 type StatsBlockProps = {
   stats: Array<{ name: string; value: number | string | null }>;
   size?: StatisticSize;
   header?: string;
+  mode: StatsMode;
 };
 
 const StatsBlock: FC<StatsBlockProps> = ({
   stats,
   size,
   header,
+  mode,
 }: StatsBlockProps) => {
   if (stats.every((item) => item.value === null)) return null;
+
+  const items = stats.map(
+    (item) =>
+      item.value && (
+        <>
+          {mode === StatsMode.Blocks && (
+            <Statistic header={item.name} size={size}>
+              {item.value}
+            </Statistic>
+          )}
+
+          {mode === StatsMode.Table && (
+            <tr>
+              <th>{item.name}</th>
+              <td>{item.value}</td>
+            </tr>
+          )}
+        </>
+      )
+  );
 
   return (
     <article>
       {header && <p className={styles["stats-header"]}>{header}</p>}
-      <div className={styles["stats"]}>
-        {stats.map(
-          (item) =>
-            item.value && (
-              <Statistic header={item.name} size={size}>
-                {item.value}
-              </Statistic>
-            )
-        )}
-      </div>
+
+      {mode === StatsMode.Blocks && (
+        <div className={styles["stats-blocks"]}>{items}</div>
+      )}
+
+      {mode === StatsMode.Table && (
+        <table className={styles["stats-table"]}>{items}</table>
+      )}
     </article>
   );
 };
@@ -50,9 +78,14 @@ export const ColumnCard: FC<ColumnCardProps> = ({
     false
   );
 
-  return (
-    <article className={styles.card}>
-      <div className={styles.header}>
+  const [mode, toggleMode] = useReducer(
+    (mode) => (mode === StatsMode.Blocks ? StatsMode.Table : StatsMode.Blocks),
+    StatsMode.Blocks
+  );
+
+  const header = (
+    <div className={styles.header}>
+      <div className={styles["name-with-badges"]}>
         <div className={styles.name}>
           <span>#{column.columnIndex + 1}</span>
           <h6>{column.columnName}</h6>
@@ -62,40 +95,72 @@ export const ColumnCard: FC<ColumnCardProps> = ({
           {column.isCategorical && <Badge>Categorical</Badge>}
         </div>
       </div>
+      <Button
+        variant="secondary"
+        icon={mode === StatsMode.Blocks ? grid : list}
+        className={styles["mode-button"]}
+        onClick={toggleMode}
+      />
+    </div>
+  );
 
-      <div className={styles.content}>
+  const content = (
+    <div
+      className={classNames(
+        styles.content,
+        mode === StatsMode.Table && styles["table-mode"]
+      )}
+    >
+      <div className={styles["basic-stats"]}>
         <StatsBlock
+          mode={mode}
           stats={[
             { name: "distinct", value: column.distinct },
             { name: "count", value: column.count },
+          ]}
+          size="big"
+        />
+        <StatsBlock
+          mode={mode}
+          stats={[
             { name: "avg", value: column.avg },
             { name: "std", value: column.STD },
           ]}
           size="big"
         />
-        <Collapse isOpened={showDetails}>
-          <div className={styles["more-stats"]}>
-            <StatsBlock
-              stats={[
-                { name: "min", value: column.min },
-                { name: "max", value: column.max },
-                { name: "quantile 25", value: column.quantile25 },
-                { name: "quantile 50", value: column.quantile50 },
-                { name: "quantile 75", value: column.quantile75 },
-              ]}
-              header="Quantile stats"
-            />
-            <StatsBlock
-              stats={[
-                { name: "skewness", value: column.skewness },
-                { name: "kurtosis", value: column.kurtosis },
-                { name: "sum", value: column.sum },
-              ]}
-              header="Descriptive stats"
-            />
-          </div>
-        </Collapse>
       </div>
+      <Collapse isOpened={showDetails}>
+        <div className={styles["more-stats"]}>
+          <StatsBlock
+            mode={mode}
+            stats={[
+              { name: "min", value: column.min },
+              { name: "max", value: column.max },
+              { name: "quantile 25", value: column.quantile25 },
+              { name: "quantile 50", value: column.quantile50 },
+              { name: "quantile 75", value: column.quantile75 },
+            ]}
+            header="Quantile stats"
+          />
+          <StatsBlock
+            mode={mode}
+            stats={[
+              { name: "skewness", value: column.skewness },
+              { name: "kurtosis", value: column.kurtosis },
+              { name: "sum", value: column.sum },
+            ]}
+            header="Descriptive stats"
+          />
+        </div>
+      </Collapse>
+    </div>
+  );
+
+  return (
+    <article className={styles.card}>
+      {header}
+
+      {content}
 
       <Button
         variant="secondary"
