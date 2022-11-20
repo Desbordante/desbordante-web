77e08be7d47fde2cc285435dcb50724981056709
Ilya <barutkinila@gmail.com>
2022-11-20 21:39:31 +0000

    Add sort & squash options

--- web-app/client/src/components/ScrollableTable/ClusterTable.tsx
+++ web-app/client/src/components/ScrollableTable/ClusterTable.tsx
@@ -1,14 +1,17 @@
 import { useLazyQuery, useQuery } from '@apollo/client';
 import { GET_SPECIFIC_CLUSTER } from '@graphql/operations/queries/EDP/getSpecificCluster';
 import _ from 'lodash';
+import { FC, useCallback, useEffect, useState } from 'react';
+import classNames from 'classnames';
+import Image from 'next/image';
 import {
   getSpecificCluster,
   getSpecificClusterVariables,
 } from '@graphql/operations/queries/EDP/__generated__/getSpecificCluster';
-import classNames from 'classnames';
-import { FC, useCallback, useEffect, useState } from 'react';
-import styles from './Table.module.scss';
+import threeDots from '@assets/icons/three-dots.svg';
 import Table from './ScrollableTable';
+import styles from './Table.module.scss';
+import { Checkbox } from '@components/Inputs';
 
 type ClusterTableProps = {
   data: string[][] | undefined;
@@ -27,6 +30,10 @@ const ClusterTable: FC<ClusterTableProps> = ({
 }) => {
   const [offset, setOffset] = useState(0);
   const [data, setData] = useState(defaultData || []);
+  const [showOptions, setShowOptions] = useState(false);
+
+  const [squash, setSquash] = useState(false);
+  const [sort, setSort] = useState(false);
 
   const [
     getSpecificCluster,
@@ -43,25 +50,33 @@ const ClusterTable: FC<ClusterTableProps> = ({
     pageResult?.taskInfo.data.result?.specificCluster &&
     'items' in pageResult?.taskInfo.data.result?.specificCluster;
 
+  const parseItem = (e: any) => e.row;
+  const parseSquashItem = (e: any) => ['x' + e.amount].concat(e.row);
   const pageRows =
     pageCompleted &&
     // @ts-ignore
-    pageResult?.taskInfo.data.result.specificCluster?.items.map((e) => e.row);
+    pageResult?.taskInfo.data.result.specificCluster?.items.map(
+      squash ? parseSquashItem : parseItem
+    );
 
   useEffect(() => {
-    // if (offset === 0) return;
+    setData((!squash && !sort && defaultData) || []);
+    setOffset(0);
+  }, [squash, sort]);
+
+  useEffect(() => {
     getSpecificCluster({
       variables: {
         taskId: specificTaskID,
         props: {
           clusterID,
-          sort: true,
-          squash: true,
+          sort,
+          squash,
         },
         pagination: { offset, limit: 20 },
       },
     });
-  }, [offset]);
+  }, [offset, sort, squash, clusterID]);
 
   useEffect(() => {
     if (pageCompleted) {
@@ -86,10 +101,46 @@ const ClusterTable: FC<ClusterTableProps> = ({
   }, [pageResult]);
 
   return (
-    <Table
-      {...{ data, header, onScroll }}
-      className={classNames(loading && styles.disabled)}
-    />
+    <div className={styles.clusterContainer}>
+      <Table
+        header={header && squash ? ['Rows count'].concat(header) : header}
+        {...{ data, onScroll }}
+        className={classNames(loading && styles.disabled)}
+      />
+
+      <div className={styles.options}>
+        <Image
+          onClick={() => setShowOptions((e) => !e)}
+          src={threeDots}
+          width={24}
+          height={24}
+        />
+        {showOptions && (
+          <div
+            onClick={() => setShowOptions(false)}
+            className={styles.optionsOverlay}
+          ></div>
+        )}
+
+        {showOptions && (
+          <div className={styles.optionsDropdown}>
+            <Checkbox
+              label="Sorted"
+              checked={sort}
+              id="ClusterTableSortedCheckbox"
+              onChange={() => setSort((sorted) => !sorted)}
+            />
+
+            <Checkbox
+              label="Squashed"
+              checked={squash}
+              id="ClusterTableSquashedCheckbox"
+              onChange={() => setSquash((squashed) => !squashed)}
+            />
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 export default ClusterTable;
