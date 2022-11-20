e39720d4243558e5abdf00360c3822167f0152c1
Ilya <barutkinila@gmail.com>
2022-11-20 20:27:54 +0000

    Refactor table component

--- web-app/client/src/components/ScrollableTable/ScrollableTable.tsx
+++ web-app/client/src/components/ScrollableTable/ScrollableTable.tsx
@@ -1,76 +1,32 @@
 import { useLazyQuery, useQuery } from '@apollo/client';
 import { GET_SPECIFIC_CLUSTER } from '@graphql/operations/queries/EDP/getSpecificCluster';
+import _ from 'lodash';
 import {
   getSpecificCluster,
   getSpecificClusterVariables,
 } from '@graphql/operations/queries/EDP/__generated__/getSpecificCluster';
 import classNames from 'classnames';
-import { FC, PropsWithChildren, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import styles from './Table.module.scss';
 
 type Props = {
   data: string[][] | undefined;
-  specificTaskID: string;
-  clusterID: number;
+  header?: string[];
+  onScroll: () => void;
+  className?: string;
 };
-const Table: FC<Props> = ({ data: defaultData, specificTaskID, clusterID }) => {
-  const {
-    data: pageResult,
-    error,
-    startPolling,
-    stopPolling,
-    refetch,
-  } = useQuery<getSpecificCluster, getSpecificClusterVariables>(
-    GET_SPECIFIC_CLUSTER,
-    { fetchPolicy: 'network-only' }
-  );
 
+const Table: FC<Props> = ({ data, header, onScroll, className }) => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [data, setData] = useState(defaultData || []);
-  const [offset, setOffset] = useState(0);
-
-  const [totalCount, pageRows] = (pageResult?.taskInfo.data &&
-    'result' in pageResult?.taskInfo.data &&
-    pageResult?.taskInfo.data.result?.specificCluster &&
-    'items' in pageResult?.taskInfo.data.result?.specificCluster && [
-      pageResult?.taskInfo.data.result?.specificCluster.itemsAmount,
-      pageResult?.taskInfo.data.result?.specificCluster?.items.map(
-        (e) => e.row
-      ),
-    ]) || [0, undefined];
 
   useEffect(() => {
-    if (!isScrolled) return;
-    if (totalCount < offset + 20) {
-      setOffset((offset) => offset + 20);
+    if (isScrolled) {
+      onScroll();
     }
   }, [isScrolled]);
-
-  useEffect(() => {
-    if (offset > 0) {
-      refetch({
-        taskId: specificTaskID,
-        props: {
-          clusterID,
-          sort: true,
-          squash: false,
-        },
-        pagination: { offset, limit: 20 },
-      });
-      startPolling(2000);
-    }
-  }, [offset]);
-
-  useEffect(() => {
-    if (pageRows) {
-      stopPolling();
-      setData(data.concat(pageRows));
-    }
-  }, [pageRows]);
-
   return (
     <div
-      className={classNames(styles.container, isScrolled && styles.disabled)}
+      className={classNames(styles.container, className)}
       onScroll={(e) =>
         setIsScrolled(
           Math.abs(
@@ -82,7 +38,10 @@ const Table: FC<Props> = ({ data: defaultData, specificTaskID, clusterID }) => {
     >
       <table className={styles.table}>
         <thead>
-          <tr>{data && data[0].map((e, i) => <td>Column {i + 1}</td>)}</tr>
+          <tr>{header && header.map((e) => <td>{e}</td>)}</tr>
+          <tr>
+            {!header && data && data[0].map((e, i) => <td>Column {i}</td>)}
+          </tr>
         </thead>
         <tbody>
           {data &&
@@ -98,4 +57,5 @@ const Table: FC<Props> = ({ data: defaultData, specificTaskID, clusterID }) => {
     </div>
   );
 };
+
 export default Table;
