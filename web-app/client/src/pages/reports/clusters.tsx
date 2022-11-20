b17cd998693bd0023548bdd6ca19a4cb5179ced3
Ilya <barutkinila@gmail.com>
2022-11-20 17:25:32 +0000

    Add basic pagination for clusters

--- web-app/client/src/pages/reports/clusters.tsx
+++ web-app/client/src/pages/reports/clusters.tsx
@@ -6,54 +6,79 @@ import { TaskContextProvider, useTaskContext } from '@components/TaskContext';
 import { NextPageWithLayout } from 'types/pageWithLayout';
 import styles from '@styles/Clusters.module.scss';
 import longArrowIcon from '@assets/icons/long-arrow.svg';
-import { useLazyQuery } from '@apollo/client';
+import { useLazyQuery, useQuery } from '@apollo/client';
 import {
   getClustersPreview,
   getClustersPreviewVariables,
   getClustersPreview_taskInfo_data_SpecificTaskData_result,
+  getClustersPreview_taskInfo_data_SpecificTaskData_result_typoClusters,
 } from '@graphql/operations/queries/EDP/__generated__/getClustersPreview';
 import { GET_CLUSTERS_PREVIEW } from '@graphql/operations/queries/EDP/getClustersPreview';
-import Table from '@components/Table';
+import ScrollableTable from '@components/ScrollableTable';
+import Pagination from '@components/Pagination/Pagination';
 
 const ReportsClusters: NextPageWithLayout = () => {
   const { selectedDependency, specificTaskID } = useTaskContext();
 
-  const [clusters, setClusters] =
-    useState<getClustersPreview_taskInfo_data_SpecificTaskData_result>();
-  const [getClustersPreview] = useLazyQuery<
+  const [page, setPage] = useState(1);
+
+  const { refetch, startPolling, stopPolling, data, error } = useQuery<
     getClustersPreview,
     getClustersPreviewVariables
-  >(GET_CLUSTERS_PREVIEW, { fetchPolicy: 'network-only' });
+  >(GET_CLUSTERS_PREVIEW, {
+    // skip: false,
+    fetchPolicy: 'network-only',
+    pollInterval: 2000,
+  });
 
   useEffect(() => {
-    if (!specificTaskID) return;
-    getClustersPreview({
-      variables: {
+    if (data) {
+      stopPolling();
+    }
+  }, [data]);
+
+  useEffect(() => {
+    stopPolling();
+    if (specificTaskID) {
+      refetch({
         taskId: specificTaskID,
-        clustersPagination: { offset: 0, limit: 500 },
-        itemsLimit: 10,
-      },
-    }).then((res) => {
-      if (
-        res.data?.taskInfo.data &&
-        'result' in res.data?.taskInfo.data &&
-        res.data?.taskInfo.data.result
-      ) {
-        setClusters(res.data?.taskInfo.data.result);
-      }
-    });
-  }, []);
+        clustersPagination: { offset: 0 /*page - 1*/, limit: 100 /*1*/ },
+        itemsLimit: 20,
+      });
+      startPolling(2000);
+    }
+  }, [page]);
+
+  const [totalCount, cluster] = (data?.taskInfo.data &&
+    'result' in data?.taskInfo.data &&
+    data?.taskInfo.data.result && [
+      data.taskInfo.data.result.clustersCount,
+      data?.taskInfo.data.result.typoClusters[page - 1],
+    ]) || [0, undefined];
+
   return (
     <div className={styles.container}>
       <h5>Clusters</h5>
-      {selectedDependency.length > 0 && !clusters && (
+      {selectedDependency.length > 0 && !cluster && (
         <Loader
           lhs={selectedDependency.slice(0, -1).map((e) => e.column.name)}
           rhs={selectedDependency.at(-1)!.column.name}
         />
       )}{' '}
-      {clusters && (
-        <Table data={clusters?.typoClusters[0]!.items.map((e) => e.row)} />
+      {specificTaskID && cluster && (
+        <>
+          <ScrollableTable
+            key={cluster.clusterID}
+            specificTaskID={specificTaskID}
+            clusterID={cluster.clusterID}
+            data={cluster.items.map((e) => e.row)}
+          />{' '}
+          <Pagination
+            count={totalCount}
+            current={page}
+            onChange={(page) => setPage(page)}
+          />
+        </>
       )}
     </div>
   );
@@ -82,7 +107,7 @@ const Loader: FC<LoaderProps> = ({ lhs, rhs }) => {
           {lhs.map((attr) => (
             <span className={styles.attr}>{attr}</span>
           ))}
-          <Image src={longArrowIcon} />
+          <Image src={longArrowIcon} width={66} height={15} />
           <span className={styles.attr}>{rhs}</span>
         </div>
       </div>
