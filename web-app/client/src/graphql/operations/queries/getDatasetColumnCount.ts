de5be991f686da4c4975a25934312b20c79ebf5e
Pechenux <belokoniy@gmail.com>
2022-12-18 15:49:26 +0300

    Fixed globals.scss after moving to the last commits. Removed commas from getDatasetColumnCount query for consistency

--- web-app/client/src/graphql/operations/queries/getDatasetColumnCount.ts
+++ web-app/client/src/graphql/operations/queries/getDatasetColumnCount.ts
@@ -3,8 +3,8 @@ import { gql } from '@apollo/client';
 export const GET_COUNT_OF_COLUMNS = gql`
   query getCountOfColumns($fileID: ID!) {
     datasetInfo(fileID: $fileID) {
-      countOfColumns,
-      hasHeader,
+      countOfColumns
+      hasHeader
       header
     }
   }
