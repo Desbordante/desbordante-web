c435d6287663c1978877c0a2f17b791b1a403922
Max Fofanov <max.fofanov@gmail.com>
2023-03-21 18:34:18 +0300

    Rename CsvWriters directory and add colDelimiter

--- web-app/server/src/graphql/schema/TaskInfo/Writers/CFDWriter.ts
+++ web-app/server/src/graphql/schema/TaskInfo/Writers/CFDWriter.ts
@@ -6,25 +6,12 @@ export class CFDWriter extends AbstractWriter<Cfd> {
         super(deps, props);
         this.header = ["LHS", "RHS", "CONFIDENCE", "SUPPORT"];
     }
-    /*
-    transformDepCsv(dep: Cfd): {
-        lhs: string;
-        rhs: string;
-        support: number;
-        confidence: number;
-    } {
-        return {
-            lhs: dep.lhs.map(({ column }) => column.name).join("|"),
-            rhs: dep.rhs.column.name,
-            support: dep.support,
-            confidence: dep.confidence,
-        };
-    }
-    */
 
     transformDep(dep: Cfd): TransformedDep {
         return [
-            dep.lhs.map(({ column, pattern }) => `${column.name}=${pattern}`).join("|"),
+            dep.lhs
+                .map(({ column, pattern }) => `${column.name}=${pattern}`)
+                .join(CFDWriter.colDelimiter),
             `${dep.rhs.column.name}=${dep.rhs.pattern}`,
             dep.confidence,
             dep.support,
