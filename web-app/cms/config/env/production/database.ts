2add14131c5d128eda17985866ef142b6090e530
Kirill Stupakov <kirill.stupakov.0@gmail.com>
2023-08-03 21:11:59 +0300

    Remove unused strapi boilerplate

--- web-app/cms/config/env/production/database.ts
+++ web-app/cms/config/env/production/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
-			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'cms'),
-			user: env('DATABASE_USERNAME', 'postgres'),
-			password: env('DATABASE_PASSWORD', 'postgres'),
-			ssl: env.bool('DATABASE_SSL', false)
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'cms'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+      ssl: env.bool('DATABASE_SSL', false)
 		}
 	}
 });
--- web-app/cms/config/plugins.ts
+++ web-app/cms/config/plugins.ts
@@ -1,18 +1,18 @@
 import path from 'path';
 
 export default ({ env }) => ({
-    graphql: {
-        enabled: true,
-        config: {
-            generateArtifacts: true,
-            shadowCRUD: true,
-            artifacts: {
-                schema: path.join(__dirname, '..', '..', 'schema.graphql'),
-                typegen: path.join(__dirname, '..', '..', 'types.d.ts'),
-            },
-            apolloServer: {
-                introspection: true,
-            },
-        },
+  graphql: {
+    enabled: true,
+    config: {
+      generateArtifacts: true,
+      shadowCRUD: true,
+      artifacts: {
+        schema: path.join(__dirname, '..', '..', 'schema.graphql'),
+        typegen: path.join(__dirname, '..', '..', 'types.d.ts'),
+      },
+      apolloServer: {
+        introspection: true,
+      },
     },
+  },
 });
