37eb004fb61dc29f3dbeb9b71f81262d654252a9
Kirill Stupakov <kirill.stupakov.0@gmail.com>
2023-08-04 13:39:15 +0300

    Fix not passing some envs to client

--- web-app/client/src/utils/env.ts
+++ web-app/client/src/utils/env.ts
@@ -1,2 +1,5 @@
 export const port = process.env.PORT;
 export const cmsApiToken = process.env.CMS_API_TOKEN;
+export const googleAnalyticsKey = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
+export const passwordSaltPrefix = process.env.NEXT_PUBLIC_PASSWORD_SALT_PREFIX;
+export const passwordSaltPostfix = process.env.NEXT_PUBLIC_PASSWORD_SALT_POSTFIX;
