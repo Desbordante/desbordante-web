509e12c0dbb0ca7dc46731f4367e535674265b82
Kirill Stupakov <kirill.stupakov.0@gmail.com>
2024-04-26 19:24:57 +0300

    Fix admin panel & user cabinet access for unauthorized users

--- web-app/client/src/pages/admin-panel/[tab].tsx
+++ web-app/client/src/pages/admin-panel/[tab].tsx
@@ -1,10 +1,27 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import TabsLayout from '@components/TabsLayout';
+import { useAuthContext } from '@hooks/useAuthContext';
 import tabs from 'src/routes/AdminPanel/tabs';
 import { NextPageWithLayout } from 'types/pageWithLayout';
 
 const AdminPanel: NextPageWithLayout = () => {
   const router = useRouter();
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    if (user === undefined) {
+      return;
+    }
+
+    if (user === null || !user.permissions.canViewAdminInfo) {
+      router.push('/');
+    }
+  }, [router, user]);
+
+  if (!user) {
+    return null;
+  }
 
   const currentTab =
     tabs.find((tab) => router.query.tab === tab.pathname) ?? tabs[0];
