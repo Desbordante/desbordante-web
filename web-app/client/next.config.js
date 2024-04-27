fbb53af2bb361b77e912e09e25bb82d482aa9083
pechenux <belokoniy@gmail.com>
2024-04-27 14:10:52 +0300

    Actions: remove cache, add build and type checks

--- web-app/client/next.config.js
+++ web-app/client/next.config.js
@@ -10,7 +10,7 @@ module.exports = {
   swcMinify: true,
   eslint: {
     dirs: ['src'],
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   typescript: {
     ignoreBuildErrors: true,
